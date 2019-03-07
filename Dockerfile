773274337751b84f8e77f492cbf1d6f1ce90c254
Vladimir Enchev <vladimir.enchev@gmail.com>
2019-03-07 16:08:57 +0200

    Demo updated to 3.0.0-preview3-19153-02

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM microsoft/dotnet:3.0-sdk
 COPY . /app
-WORKDIR /app/WebApplication1/WebApplication1.Server
+WORKDIR /app/RazorComponentsApp1
 
 ENV ASPNETCORE_URLS http://*:5000
  
--- RazorComponentsApp1/Program.cs
+++ RazorComponentsApp1/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace WebApplication1.Server
+namespace RazorComponentsApp1
 {
     public class Program
     {
