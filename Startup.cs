5e3fa26efcecb01637aebce28ea72cc0ee39a351
Atanas Korchev <akorchev@gmail.com>
2019-08-16 18:36:14 +0300

    Run production build in the Docker container.

--- Startup.cs
+++ Startup.cs
@@ -42,6 +42,7 @@ namespace LatestBlazor
       {
         app.UseDeveloperExceptionPage();
       }
+      /*
       else
       {
         // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -53,6 +54,7 @@ namespace LatestBlazor
       }
 
       app.UseHttpsRedirection();
+      */
 
       app.UseStaticFiles();
 
