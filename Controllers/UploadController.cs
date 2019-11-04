f2ded7ba1f62e670f3181d330df1408337258a3a
Vladimir Enchev <vladimir.enchev@gmail.com>
2019-11-04 16:53:32 +0200

    Upload added

---
+++ Controllers/UploadController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace LatestBlazor
+{
+    public partial class UploadController : Controller
+    {
+        [HttpPost("upload/single")]
+        public IActionResult Single(IFormFile file)
+        {
+            try
+            {
+                // Put your code here
+                return StatusCode(200);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost("upload/multiple")]
+        public IActionResult Multiple(IFormFile[] files)
+        {
+            try
+            {
+                // Put your code here
+                return StatusCode(200);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost("upload/{id}")]
+        public IActionResult Post(IFormFile[] files, int id)
+        {
+            try
+            {
+                // Put your code here
+                return StatusCode(200);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
