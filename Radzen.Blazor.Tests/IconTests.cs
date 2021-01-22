173bf45bec2e8f3ff58608d74b3769de5d168783
Vladimir Enchev <vladimir.enchev@gmail.com>
2021-01-22 10:55:07 +0200

    SplitButton, Icon, Image and Link tests added

---
+++ Radzen.Blazor.Tests/IconTests.cs
@@ -0,0 +1,35 @@
+using Bunit;
+using Xunit;
+
+namespace Radzen.Blazor.Tests
+{
+    public class IconTests
+    {
+        [Fact]
+        public void Icon_Renders_IconParameter()
+        {
+            using var ctx = new TestContext();
+
+            var component = ctx.RenderComponent<RadzenIcon>();
+
+            var icon = "account_circle";
+
+            component.SetParametersAndRender(parameters => parameters.Add(p => p.Icon, icon));
+
+            Assert.Contains(@$">{icon}</i>", component.Markup);
+            Assert.Contains(@$"<i class=""rzi d-inline-flex justify-content-center align-items-center""", component.Markup);
+        }
+
+        [Fact]
+        public void Icon_Renders_UnmatchedParameter()
+        {
+            using var ctx = new TestContext();
+
+            var component = ctx.RenderComponent<RadzenIcon>();
+
+            component.SetParametersAndRender(parameters => parameters.AddUnmatched("autofocus", ""));
+
+            Assert.Contains(@$"autofocus", component.Markup);
+        }
+    }
+}
