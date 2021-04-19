85c8b636a8f1895afa7dfb6e8222649b9b4c8392
Atanas Korchev <akorchev@gmail.com>
2021-04-19 13:43:04 +0300

    Improve Scheduler monthly view event rendering to take advantage of remaining space.

--- Radzen.Blazor/AppointmentData.cs
+++ Radzen.Blazor/AppointmentData.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Radzen.Blazor
 {
@@ -8,5 +9,19 @@ namespace Radzen.Blazor
         public DateTime End { get; set; }
         public string Text { get; set; }
         public object Data { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is AppointmentData data &&
+                   Start == data.Start &&
+                   End == data.End &&
+                   Text == data.Text &&
+                   EqualityComparer<object>.Default.Equals(Data, data.Data);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Start, End, Text, Data);
+        }
     }
 }
