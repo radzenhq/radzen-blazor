349eb433ce92e81e8107a0d8538dde4a8afa0d64
Vladimir Enchev <vladimir.enchev@gmail.com>
2021-06-21 15:49:08 +0300

    Tree CheckBoxes support added (#153)

--- Radzen.Blazor/QueryableExtension.cs
+++ Radzen.Blazor/QueryableExtension.cs
@@ -676,5 +676,15 @@ namespace Radzen
         {
             return new ODataEnumerable<T>(source);
         }
+
+        public static IEnumerable<T> SelectManyRecursive<T>(this IEnumerable<T> source, Func<T, IEnumerable<T>> selector)
+        {
+            var result = source.SelectMany(selector);
+            if (!result.Any())
+            {
+                return result;
+            }
+            return result.Concat(result.SelectManyRecursive(selector));
+        }
     }
 }
