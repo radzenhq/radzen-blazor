c6f0a8825c3b8b11f3b89e2fd249081b4b2bf946
Vitaliy Orazov <viordash@mail.ru>
2021-07-12 02:32:45 +0300

    WIP. add test for DataGridColumn filter property type

---
+++ Radzen.Blazor.Tests/DataGridColumnTests.cs
@@ -0,0 +1,41 @@
+using AngleSharp.Dom;
+using Bunit;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Radzen.Blazor.Rendering;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Xunit;
+
+namespace Radzen.Blazor.Tests
+{
+    public class DataGridColumnTests
+    {
+        class Testable<TItem> : RadzenDataGridColumn<TItem> 
+        {
+			public Testable() {
+                Grid = new RadzenDataGrid<TItem>();
+            }
+            public void PublicMorozov_OnInitialized() 
+            {
+                OnInitialized();
+            }
+        }
+
+        [Fact]
+        public void DataGridColumn_FilterPropertyType_Used_From_Type_Parameter()
+        {
+            
+            var column = new Testable<int>() 
+            { 
+                Type = typeof(string)
+            };
+
+            column.PublicMorozov_OnInitialized();
+            Assert.Equal(column.FilterOperator, FilterOperator.Contains);
+        }
+    }
+}
