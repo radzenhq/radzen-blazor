0b638a685778b0a363d00048369e5e0fcc67b369
Vitaliy Orazov <viordash@mail.ru>
2021-07-13 00:41:05 +0300

    assign filterPropertyType from Type only when FilterProperty is empty, otherwise assign filter type from FilterProperty field && add related tests

--- Radzen.Blazor.Tests/DataGridColumnTests.cs
+++ Radzen.Blazor.Tests/DataGridColumnTests.cs
@@ -14,28 +14,93 @@ namespace Radzen.Blazor.Tests
 {
     public class DataGridColumnTests
     {
-        class Testable<TItem> : RadzenDataGridColumn<TItem> 
+        class TestModel
         {
-			public Testable() {
-                Grid = new RadzenDataGrid<TItem>();
+            public Guid Id { get; set; }
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+        class Testable : RadzenDataGridColumn<TestModel>
+        {
+            public Testable()
+            {
+                Grid = new RadzenDataGrid<TestModel>();
             }
-            public void PublicMorozov_OnInitialized() 
+            public void PublicMorozov_OnInitialized()
             {
                 OnInitialized();
+            }
+            public Type PublicMorozov_FilterPropertyType()
+            {
+                var propertyInfo = this.GetType()
+                    .GetProperties(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                    .FirstOrDefault(x => x.Name == "FilterPropertyType");
+
+                return propertyInfo?.GetValue(this) as Type;
             }
         }
 
         [Fact]
-        public void DataGridColumn_FilterPropertyType_Used_From_Type_Parameter()
+        public void FilterPropertyType_Assigned_From_Type_Parameter()
         {
-            
-            var column = new Testable<int>() 
-            { 
-                Type = typeof(string)
+            var column = new Testable()
+            {
+                Property = nameof(TestModel.Id),
+                Type = typeof(string),
+                FilterProperty = null
             };
 
             column.PublicMorozov_OnInitialized();
-            Assert.Equal(column.FilterOperator, FilterOperator.Contains);
+
+            Assert.Equal(typeof(string), column.PublicMorozov_FilterPropertyType());
+            Assert.Equal(FilterOperator.Contains, column.FilterOperator);
+        }
+
+        [Fact]
+        public void FilterPropertyType_Assigned_From_FilterProperty_Parameter()
+        {
+            var column = new Testable()
+            {
+                Property = nameof(TestModel.Id),
+                Type = null,
+                FilterProperty = nameof(TestModel.Name)
+            };
+
+            column.PublicMorozov_OnInitialized();
+
+            Assert.Equal(typeof(string), column.PublicMorozov_FilterPropertyType());
+            Assert.Equal(FilterOperator.Contains, column.FilterOperator);
+        }
+
+        [Fact]
+        public void FilterPropertyType_Assigned_From_ColumnType()
+        {
+            var column = new Testable()
+            {
+                Property = nameof(TestModel.Id),
+                Type = null,
+                FilterProperty = null
+            };
+
+            column.PublicMorozov_OnInitialized();
+
+            Assert.Equal(typeof(Guid), column.PublicMorozov_FilterPropertyType());
+            Assert.Equal(FilterOperator.Equals, column.FilterOperator);
+        }
+
+        [Fact]
+        public void FilterPropertyType_Assigned_From_Type_If_FilterProperty_Is_Fake_Field()
+        {
+            var column = new Testable()
+            {
+                Property = nameof(TestModel.Id),
+                Type = typeof(decimal),
+                FilterProperty = "NotExistsField"
+            };
+
+            column.PublicMorozov_OnInitialized();
+
+            Assert.Equal(typeof(decimal), column.PublicMorozov_FilterPropertyType());
         }
     }
 }
