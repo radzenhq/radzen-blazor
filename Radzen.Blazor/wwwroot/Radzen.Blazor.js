11da78875651033498668995a2c98a5ac753d901
Vladimir Enchev <vladimir.enchev@gmail.com>
2021-09-29 09:00:13 +0300

    DropDown with filtering closed immediately when open on Android
    Fix #233

--- Radzen.Blazor/wwwroot/Radzen.Blazor.js
+++ Radzen.Blazor/wwwroot/Radzen.Blazor.js
@@ -726,7 +726,7 @@ window.Radzen = {
     }
 
     Radzen[id] = function (e) {
-        if (e.type == 'resize') {
+        if (!/Android/i.test(navigator.userAgent) && e.type == 'resize') {
             Radzen.closePopup(id, instance, callback);
             return;
         }
