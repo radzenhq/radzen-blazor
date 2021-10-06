72c07c4cd47e41f402432a226b8efdafa485d444
Vladimir Enchev <vladimir.enchev@gmail.com>
2021-10-06 12:00:41 +0300

    RadzenCard reworked with partial class

---
+++ Radzen.Blazor/RadzenCard.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Threading.Tasks;
+
+namespace Radzen.Blazor
+{
+    public partial class RadzenCard : RadzenComponentWithChildren
+    {
+        protected override string GetComponentCssClass()
+        {
+            return "rz-card card";
+        }
+    }
+}
