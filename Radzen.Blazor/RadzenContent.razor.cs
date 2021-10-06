8320681e9246b3ab182fde0da72466d401cc12de
Vladimir Enchev <vladimir.enchev@gmail.com>
2021-10-06 12:34:17 +0300

    RadzenContent reworked with partial class

---
+++ Radzen.Blazor/RadzenContent.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Radzen.Blazor
+{
+    public partial class RadzenContent : RadzenComponentWithChildren
+    {
+        [Parameter]
+        public string Container { get; set; }
+
+        protected override string GetComponentCssClass()
+        {
+            return "content";
+        }
+    }
+}
