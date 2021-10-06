db170b67ad9274d51e8c06dc7a5bad383b67fde6
Vladimir Enchev <vladimir.enchev@gmail.com>
2021-10-06 12:23:54 +0300

    RadzenColorPicker reworked with partial class

---
+++ Radzen.Blazor/RadzenColorPicker.razor.cs
@@ -0,0 +1,335 @@
+using Microsoft.AspNetCore.Components;
+using Radzen.Blazor.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Radzen.Blazor
+{
+    public partial class RadzenColorPicker : FormComponent<string>
+    {
+        [Parameter]
+        public EventCallback Open { get; set; }
+
+        [Parameter]
+        public EventCallback Close { get; set; }
+
+        [Parameter]
+        public string Icon { get; set; }
+
+        [Parameter]
+        public string HexText { get; set; } = "Hex";
+
+        [Parameter]
+        public string RedText { get; set; } = "R";
+
+        [Parameter]
+        public string GreenText { get; set; } = "G";
+
+        [Parameter]
+        public string BlueText { get; set; } = "B";
+
+        [Parameter]
+        public string AlphaText { get; set; } = "A";
+
+        [Parameter]
+        public string ButtonText { get; set; } = "OK";
+
+        Popup Popup { get; set; }
+
+        string AlphaGradientStart
+        {
+            get
+            {
+                var rgb = RGB.Parse(Color);
+                rgb.Alpha = 0;
+                return rgb.ToCSS();
+            }
+        }
+
+        string AlphaGradientEnd
+        {
+            get
+            {
+                var rgb = RGB.Parse(Color);
+                rgb.Alpha = 1;
+                return rgb.ToCSS();
+            }
+        }
+
+        string Hex
+        {
+            get
+            {
+                var rgb = RGB.Parse(Color);
+
+                if (rgb != null)
+                {
+                    return rgb.ToHex();
+                }
+
+                return String.Empty;
+            }
+        }
+
+
+        double Red
+        {
+            get
+            {
+                var rgb = RGB.Parse(Color);
+                return rgb.Red;
+            }
+        }
+
+        double Alpha
+        {
+            get
+            {
+                return Math.Round(AlphaHandleLeft * 100);
+            }
+        }
+
+        double Green
+        {
+            get
+            {
+                var rgb = RGB.Parse(Color);
+                return rgb.Green;
+            }
+        }
+
+        double Blue
+        {
+            get
+            {
+                var rgb = RGB.Parse(Color);
+                return rgb.Blue;
+            }
+        }
+
+
+        void OnSaturationMove(DraggableEventArgs args)
+        {
+            SaturationHandleLeft = Math.Clamp((args.ClientX - args.Rect.Left) / args.Rect.Width, 0, 1);
+            SaturationHandleTop = Math.Clamp((args.ClientY - args.Rect.Top) / args.Rect.Height, 0, 1);
+
+            var hsv = new HSV { Hue = HSV.Hue, Saturation = SaturationHandleLeft, Value = 1 - SaturationHandleTop, Alpha = AlphaHandleLeft };
+
+            Color = hsv.ToRGB().ToCSS();
+
+            TriggerChange();
+        }
+
+        void TriggerChange()
+        {
+            if (!ShowButton)
+            {
+                ValueChanged.InvokeAsync(Color);
+                Change.InvokeAsync(Color);
+            }
+
+            StateHasChanged();
+        }
+
+        void ChangeRGB(object value)
+        {
+            SetValue(value as string);
+        }
+
+        void SetValue(string value)
+        {
+            var rgb = RGB.Parse(value);
+
+            if (rgb != null)
+            {
+                Color = rgb.ToCSS();
+                UpdateColor(rgb);
+            }
+        }
+
+        internal async Task SelectColor(string value)
+        {
+            SetValue(value);
+
+            if (!ShowButton)
+            {
+                await Popup.CloseAsync();
+            }
+        }
+
+        void UpdateColor(RGB rgb)
+        {
+            Color = rgb.ToCSS();
+
+            HSV = rgb.ToHSV();
+
+            SaturationHandleLeft = HSV.Saturation;
+            SaturationHandleTop = 1 - HSV.Value;
+            HueHandleLeft = HSV.Hue;
+
+            TriggerChange();
+        }
+
+        void ChangeAlpha(double value)
+        {
+            if (value >= 0 && value <= 100)
+            {
+                var rgb = RGB.Parse(Color);
+                AlphaHandleLeft = rgb.Alpha = Math.Round(value / 100, 2);
+
+                Color = rgb.ToCSS();
+
+                TriggerChange();
+            }
+        }
+
+        void ChangeAlpha(object alpha)
+        {
+            if (Double.TryParse((string)alpha, out var value))
+            {
+                ChangeAlpha(value);
+            }
+        }
+
+        void ChangeColor(double value, Action<RGB, double> update)
+        {
+            if (value >= 0 && value <= 255)
+            {
+                var rgb = RGB.Parse(Color);
+
+                update(rgb, value);
+
+                UpdateColor(rgb);
+            }
+        }
+
+        void ChangeColor(object color, Action<RGB, double> update)
+        {
+            if (Double.TryParse((string)color, out var value))
+            {
+                ChangeColor(value, update);
+            }
+        }
+
+        void OnAlphaMove(DraggableEventArgs args)
+        {
+            AlphaHandleLeft = Math.Round(Math.Clamp((args.ClientX - args.Rect.Left) / args.Rect.Width, 0, 1), 2);
+
+            HSV.Alpha = AlphaHandleLeft;
+
+            var hsv = new HSV { Hue = HSV.Hue, Saturation = SaturationHandleLeft, Value = 1 - SaturationHandleTop, Alpha = AlphaHandleLeft };
+
+            Color = hsv.ToRGB().ToCSS();
+
+            TriggerChange();
+        }
+
+        void OnHueMove(DraggableEventArgs args)
+        {
+            HueHandleLeft = Math.Clamp((args.ClientX - args.Rect.Left) / args.Rect.Width, 0, 1);
+
+            HSV.Hue = HueHandleLeft;
+            var hsv = new HSV { Hue = HSV.Hue, Saturation = SaturationHandleLeft, Value = 1 - SaturationHandleTop, Alpha = AlphaHandleLeft };
+
+            Color = hsv.ToRGB().ToCSS();
+
+            TriggerChange();
+        }
+
+        async Task OnClick()
+        {
+            await ValueChanged.InvokeAsync(Color);
+            await Change.InvokeAsync(Color);
+            await Popup.CloseAsync();
+        }
+
+        [Parameter]
+        public bool ShowButton { get; set; }
+
+        [Parameter]
+        public bool ShowHSV { get; set; } = true;
+
+        [Parameter]
+        public bool ShowRGBA { get; set; } = true;
+
+        [Parameter]
+        public bool ShowColors { get; set; } = true;
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        double SaturationHandleLeft { get; set; }
+        double HueHandleLeft { get; set; }
+        double AlphaHandleLeft { get; set; } = 1;
+        double SaturationHandleTop { get; set; }
+
+        HSV HSV { get; set; } = new HSV { Hue = 0, Saturation = 1, Value = 1 };
+
+        string Color { get; set; } = "rgb(255, 255, 255)";
+
+        async Task Toggle()
+        {
+            if (!Disabled)
+            {
+                await Popup.ToggleAsync(Element);
+            }
+        }
+        protected override string GetComponentCssClass()
+        {
+            var classList = new List<string>() { "rz-colorpicker" };
+
+            if (Disabled)
+            {
+                classList.Add("rz-disabled");
+            }
+
+            return string.Join(" ", classList);
+        }
+
+        protected override void OnInitialized()
+        {
+            Init();
+
+            base.OnInitialized();
+        }
+
+        void Init()
+        {
+            var value = Value;
+            if (String.IsNullOrEmpty(Value))
+            {
+                value = "rgb(255, 255, 255)";
+            }
+
+            if (value != Color)
+            {
+                Color = value;
+
+                HSV = RGB.Parse(Color).ToHSV();
+                SaturationHandleLeft = HSV.Saturation;
+                SaturationHandleTop = 1 - HSV.Value;
+                HSV.Saturation = 1;
+                HSV.Value = 1;
+                HueHandleLeft = HSV.Hue;
+
+                if (value.StartsWith("rgba"))
+                {
+                    AlphaHandleLeft = HSV.Alpha;
+                }
+            }
+        }
+
+        public override async Task SetParametersAsync(ParameterView parameters)
+        {
+            var valueChanged = parameters.DidParameterChange(nameof(Value), Value);
+
+            await base.SetParametersAsync(parameters);
+
+            if (valueChanged)
+            {
+                Init();
+            }
+        }
+    }
+}
---
+++ Radzen.Blazor/RadzenColorPickerItem.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using Radzen.Blazor.Rendering;
+using System.Threading.Tasks;
+
+namespace Radzen.Blazor
+{
+    public partial class RadzenColorPickerItem
+    {
+        [Parameter]
+        public string Value { get; set; }
+
+        string Background
+        {
+            get
+            {
+                RGB rgb = RGB.Parse(Value);
+
+                return rgb?.ToCSS();
+            }
+        }
+
+        [CascadingParameter]
+        public RadzenColorPicker ColorPicker { get; set; }
+
+        async Task OnClick()
+        {
+            await ColorPicker.SelectColor(Value);
+        }
+    }
+}
