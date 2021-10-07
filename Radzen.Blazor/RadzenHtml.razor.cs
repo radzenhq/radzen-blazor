123f907f8396b40acdb9523a76d776afd46e584b
Vladimir Enchev <vladimir.enchev@gmail.com>
2021-10-07 09:41:16 +0300

    RadzenHtml reworked with partial class

---
+++ Radzen.Blazor/RadzenHtml.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Radzen.Blazor
+{
+    public partial class RadzenHtml : ComponentBase
+    {
+        [Parameter]
+        public bool Visible { get; set; } = true;
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
