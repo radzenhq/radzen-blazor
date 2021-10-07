48179125f44fcac42d41a7d34ce76938938c723f
Vladimir Enchev <vladimir.enchev@gmail.com>
2021-10-07 09:54:04 +0300

    RadzenLink reworked with partial class

---
+++ Radzen.Blazor/RadzenLink.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Radzen.Blazor
+{
+    public partial class RadzenLink : RadzenComponent
+    {
+        protected override string GetComponentCssClass()
+        {
+            return "rz-link";
+        }
+
+        [Parameter]
+        public string Target { get; set; }
+
+        [Parameter]
+        public string Icon { get; set; }
+
+        [Parameter]
+        public string Text { get; set; } = "";
+
+        [Parameter]
+        public string Path { get; set; } = "";
+    }
+}
