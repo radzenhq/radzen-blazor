319d8bdde4a69dfb820d98aaade98a11ab9a0fcb
Vladimir Enchev <vladimir.enchev@gmail.com>
2021-10-07 10:02:29 +0300

    RadzenMask reworked with partial class

---
+++ Radzen.Blazor/RadzenMask.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Radzen.Blazor
+{
+    public partial class RadzenMask : FormComponent<string>
+    {
+        [Parameter]
+        public bool ReadOnly { get; set; }
+
+        [Parameter]
+        public bool AutoComplete { get; set; } = true;
+
+        [Parameter]
+        public long? MaxLength { get; set; }
+
+        [Parameter]
+        public string Mask { get; set; }
+
+        [Parameter]
+        public string Pattern { get; set; }
+
+        protected async System.Threading.Tasks.Task OnChange(ChangeEventArgs args)
+        {
+            Value = $"{args.Value}";
+
+            await ValueChanged.InvokeAsync(Value);
+            if (FieldIdentifier.FieldName != null) { EditContext?.NotifyFieldChanged(FieldIdentifier); }
+            await Change.InvokeAsync(Value);
+        }
+
+        protected override string GetComponentCssClass()
+        {
+            return GetClassList("rz-textbox").ToString();
+        }
+    }
+}
