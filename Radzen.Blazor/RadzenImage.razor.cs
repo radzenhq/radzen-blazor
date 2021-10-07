1a52d98966595c9f762fac2439a8bd0637f6ded5
Vladimir Enchev <vladimir.enchev@gmail.com>
2021-10-07 09:47:17 +0300

    RadzenImage reworked with partial class

---
+++ Radzen.Blazor/RadzenImage.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Radzen.Blazor
+{
+    public partial class RadzenImage : RadzenComponentWithChildren
+    {
+        [Parameter]
+        public string Path { get; set; }
+
+        [Parameter]
+        public EventCallback<MouseEventArgs> Click { get; set; }
+
+        protected async System.Threading.Tasks.Task OnClick(MouseEventArgs args)
+        {
+            await Click.InvokeAsync(args);
+        }
+    }
+}
