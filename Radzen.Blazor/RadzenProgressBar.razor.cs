9f9376e7bff722d3189d4e795ada1706afa69d18
Vladimir Enchev <vladimir.enchev@gmail.com>
2021-10-07 10:21:35 +0300

    RadzenProgressBar reworked with partial class

---
+++ Radzen.Blazor/RadzenProgressBar.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace Radzen.Blazor
+{
+    public partial class RadzenProgressBar : RadzenComponent
+    {
+        protected override string GetComponentCssClass()
+        {
+            return Mode == ProgressBarMode.Determinate ? "rz-progressbar rz-progressbar-determinate" : "rz-progressbar rz-progressbar-indeterminate";
+        }
+
+        [Parameter]
+        public ProgressBarMode Mode { get; set; }
+
+        [Parameter]
+        public string Unit { get; set; } = "%";
+
+        [Parameter]
+        public double Value { get; set; }
+
+        [Parameter]
+        public double Max { get; set; } = 100;
+
+        [Parameter]
+        public bool ShowValue { get; set; } = true;
+
+        [Parameter]
+        public Action<double> ValueChanged { get; set; }
+    }
+}
