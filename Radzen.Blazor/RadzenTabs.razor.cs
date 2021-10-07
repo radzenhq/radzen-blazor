fd63f74b989ad97b352f2c513abcfd9e03d1227a
Vladimir Enchev <vladimir.enchev@gmail.com>
2021-10-07 11:03:33 +0300

    RadzenTabs reworked with partial class

---
+++ Radzen.Blazor/RadzenTabs.razor.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Radzen.Blazor
+{
+    public partial class RadzenTabs : RadzenComponent
+    {
+        [Parameter]
+        public TabRenderMode RenderMode { get; set; } = TabRenderMode.Server;
+
+        [Parameter]
+        public int SelectedIndex { get; set; } = -1;
+
+        private int selectedIndex = -1;
+
+        [Parameter]
+        public EventCallback<int> SelectedIndexChanged { get; set; }
+
+        [Parameter]
+        public EventCallback<int> Change { get; set; }
+
+        [Parameter]
+        public RenderFragment Tabs { get; set; }
+
+        List<RadzenTabsItem> tabs = new List<RadzenTabsItem>();
+
+        public async Task AddTab(RadzenTabsItem tab)
+        {
+            if (!tabs.Contains(tab))
+            {
+                tabs.Add(tab);
+
+                if (tab.Selected)
+                {
+                    selectedIndex = IndexOf(tab);
+                }
+
+                if (IsSelected(tab))
+                {
+                    await SelectTab(tab);
+                }
+                else if (selectedIndex < 0)
+                {
+                    await SelectTab(tab); // Select the first tab by default
+                }
+            }
+        }
+
+        internal string Id
+        {
+            get
+            {
+                return GetId();
+            }
+        }
+
+        RadzenTabsItem SelectedTab
+        {
+            get
+            {
+                return tabs.ElementAtOrDefault(selectedIndex);
+            }
+        }
+
+        public void RemoveItem(RadzenTabsItem item)
+        {
+            if (tabs.Contains(item))
+            {
+                tabs.Remove(item);
+
+                try
+                {
+                    InvokeAsync(StateHasChanged);
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        public void Reload()
+        {
+            StateHasChanged();
+        }
+
+        internal bool IsSelected(RadzenTabsItem tab)
+        {
+            return IndexOf(tab) == selectedIndex;
+        }
+
+        internal int IndexOf(RadzenTabsItem tab)
+        {
+            return tabs.IndexOf(tab);
+        }
+
+        internal async Task SelectTab(RadzenTabsItem tab, bool raiseChange = false)
+        {
+            selectedIndex = IndexOf(tab);
+
+            if (raiseChange)
+            {
+                await Change.InvokeAsync(selectedIndex);
+
+                await SelectedIndexChanged.InvokeAsync(selectedIndex);
+            }
+
+            StateHasChanged();
+        }
+
+        protected override string GetComponentCssClass()
+        {
+            return "rz-tabview";
+        }
+
+        protected override void OnInitialized()
+        {
+            selectedIndex = SelectedIndex;
+
+            base.OnInitialized();
+        }
+
+        public override async Task SetParametersAsync(ParameterView parameters)
+        {
+            if (parameters.DidParameterChange(nameof(SelectedIndex), SelectedIndex))
+            {
+                selectedIndex = parameters.GetValueOrDefault<int>(nameof(SelectedIndex));
+            }
+
+            await base.SetParametersAsync(parameters);
+        }
+
+        bool shouldRender = true;
+        protected override bool ShouldRender()
+        {
+            return shouldRender;
+        }
+
+        internal async System.Threading.Tasks.Task SelectTabOnClient(RadzenTabsItem tab)
+        {
+            var index = IndexOf(tab);
+            if (index != selectedIndex)
+            {
+                selectedIndex = index;
+
+                await JSRuntime.InvokeVoidAsync("Radzen.selectTab", $"{GetId()}-tabpanel-{selectedIndex}", selectedIndex);
+
+                shouldRender = false;
+                await Change.InvokeAsync(selectedIndex);
+                await SelectedIndexChanged.InvokeAsync(selectedIndex);
+                shouldRender = true;
+            }
+        }
+    }
+}
---
+++ Radzen.Blazor/RadzenTabsItem.razor.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Components;
+using Radzen.Blazor.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Radzen.Blazor
+{
+    public partial class RadzenTabsItem : IDisposable
+    {
+        [Parameter(CaptureUnmatchedValues = true)]
+        public IDictionary<string, object> Attributes { get; set; }
+
+        [Parameter]
+        public string Style { get; set; }
+
+        [Parameter]
+        public bool Visible { get; set; } = true;
+
+        [Parameter]
+        public string Text { get; set; }
+
+        [Parameter]
+        public RenderFragment<RadzenTabsItem> Template { get; set; }
+
+        [Parameter]
+        public string Icon { get; set; }
+
+        [Parameter]
+        public bool Selected { get; set; }
+
+        [Parameter]
+        public bool Disabled { get; set; }
+
+        ClassList ClassList => ClassList.Create("rz-state-active")
+                                        .Add("rz-tabview-selected", IsSelected)
+                                        .AddDisabled(Disabled)
+                                        .Add(Attributes);
+
+        public int Index
+        {
+            get
+            {
+                return Tabs.IndexOf(this);
+            }
+        }
+
+        public bool IsSelected
+        {
+            get
+            {
+                return Tabs.IsSelected(this);
+            }
+        }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [CascadingParameter]
+        public RadzenTabs Tabs { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await Tabs.AddTab(this);
+        }
+
+        async Task OnClick()
+        {
+            if (!Disabled)
+            {
+                if (Tabs.RenderMode == TabRenderMode.Server)
+                {
+                    await Tabs.SelectTab(this, true);
+                }
+                else
+                {
+                    await Tabs.SelectTabOnClient(this);
+                }
+            }
+        }
+
+        public override async Task SetParametersAsync(ParameterView parameters)
+        {
+            var selectedChanged = parameters.DidParameterChange(nameof(Selected), Selected);
+            var visibleChanged = parameters.DidParameterChange(nameof(Visible), Visible);
+
+            await base.SetParametersAsync(parameters);
+
+            if (selectedChanged)
+            {
+                if (Selected)
+                {
+                    Tabs?.SelectTab(this);
+                }
+            }
+
+            if (visibleChanged && IsSelected)
+            {
+                Tabs?.SelectTab(this);
+            }
+        }
+
+        public void Dispose()
+        {
+            Tabs?.RemoveItem(this);
+        }
+    }
+}
