4759724d2bbe12338df4d401feb59a3fc0f39729
Vladimir Enchev <vladimir.enchev@gmail.com>
2021-10-07 11:05:56 +0300

    RadzenTextBox reworked with partial class

---
+++ Radzen.Blazor/RadzenTextBox.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Radzen.Blazor
+{
+    public partial class RadzenTextBox : FormComponent<string>
+    {
+        [Parameter]
+        public bool ReadOnly { get; set; }
+
+        [Parameter]
+        public bool AutoComplete { get; set; } = true;
+
+        [Parameter]
+        public long? MaxLength { get; set; }
+
+        protected async System.Threading.Tasks.Task OnChange(ChangeEventArgs args)
+        {
+            Value = $"{args.Value}";
+
+            await ValueChanged.InvokeAsync(Value);
+            if (FieldIdentifier.FieldName != null) { EditContext?.NotifyFieldChanged(FieldIdentifier); }
+            await Change.InvokeAsync(Value);
+        }
+
+        protected override string GetComponentCssClass()
+        {
+            return GetClassList("rz-textbox").ToString();
+        }
+    }
+}
