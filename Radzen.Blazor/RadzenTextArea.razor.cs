96086c1b6e63e23f8488021d6358202edb4443db
Vladimir Enchev <vladimir.enchev@gmail.com>
2021-10-07 11:04:41 +0300

    RadzenTextArea reworked with partial class

---
+++ Radzen.Blazor/RadzenTextArea.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Radzen.Blazor
+{
+    public partial class RadzenTextArea : FormComponent<string>
+    {
+        [Parameter]
+        public long? MaxLength { get; set; }
+
+        [Parameter]
+        public bool ReadOnly { get; set; }
+
+        [Parameter]
+        public int Rows { get; set; } = 2;
+
+        [Parameter]
+        public int Cols { get; set; } = 20;
+
+        protected async System.Threading.Tasks.Task OnChange(ChangeEventArgs args)
+        {
+            Value = $"{args.Value}";
+
+            await ValueChanged.InvokeAsync(Value);
+            if (FieldIdentifier.FieldName != null) { EditContext?.NotifyFieldChanged(FieldIdentifier); }
+            await Change.InvokeAsync(Value);
+        }
+
+        protected override string GetComponentCssClass()
+        {
+            return GetClassList("rz-textarea").ToString();
+        }
+    }
+}
