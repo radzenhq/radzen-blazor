2e1936df52c0ae5e2d539a8f44ff397783686660
Vladimir Enchev <vladimir.enchev@gmail.com>
2021-10-07 09:48:24 +0300

    RadzenLabel reworked with partial class

---
+++ Radzen.Blazor/RadzenLabel.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Radzen.Blazor
+{
+    public partial class RadzenLabel : RadzenComponent
+    {
+        [Parameter]
+        public string Component { get; set; }
+
+        [Parameter]
+        public string Text { get; set; } = "";
+    }
+}
