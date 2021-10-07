d7e6a145e96d3a04944a549d7531bc6e1496e8cf
Vladimir Enchev <vladimir.enchev@gmail.com>
2021-10-07 10:59:01 +0300

    RadzenSwitch reworked with partial class

---
+++ Radzen.Blazor/RadzenSwitch.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using System.Threading.Tasks;
+
+namespace Radzen.Blazor
+{
+    public partial class RadzenSwitch : FormComponent<bool>
+    {
+        protected override string GetComponentCssClass()
+        {
+            return GetClassList("rz-switch").Add("rz-switch-checked", Value).ToString();
+        }
+
+        public async Task OnMouseUp(MouseEventArgs args)
+        {
+            await Toggle();
+        }
+
+        async System.Threading.Tasks.Task Toggle()
+        {
+            if (Disabled)
+            {
+                return;
+            }
+
+            Value = !Value;
+
+            await ValueChanged.InvokeAsync(Value);
+            if (FieldIdentifier.FieldName != null) { EditContext?.NotifyFieldChanged(FieldIdentifier); }
+            await Change.InvokeAsync(Value);
+        }
+    }
+}
