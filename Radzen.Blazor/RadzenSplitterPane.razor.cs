a18a959ef6d9d8bc3c4e38a17ada1c52a640d773
Vladimir Enchev <vladimir.enchev@gmail.com>
2021-10-07 10:50:34 +0300

    RadzenSplitter reworked with partial class

---
+++ Radzen.Blazor/RadzenSplitterPane.razor.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Linq;
+
+namespace Radzen.Blazor
+{
+    public partial class RadzenSplitterPane : RadzenComponent
+    {
+        RadzenSplitter _splitter;
+        private string _size;
+
+        internal string SizeRuntine { get; set; }
+
+        internal bool SizeAuto { get; set; }
+
+        internal int Index { get; set; }
+
+        internal bool IsLastResizable
+        {
+            get { return Splitter.Panes.Last(o => o.Resizable && !o.Collapsed) == this; }
+        }
+
+        internal bool IsLast => Splitter.Panes.Count - 1 == Index;
+
+        internal RadzenSplitterPane Next()
+        {
+            return Index <= Splitter.Panes.Count - 2
+                ? Splitter.Panes[Index + 1]
+                : null;
+        }
+
+        internal bool IsResizable
+        {
+            get
+            {
+                var paneNext = Next();
+
+                if (Collapsed
+                    || (Index == Splitter.Panes.Count - 2 && !paneNext.IsResizable)
+                    || (IsLastResizable && paneNext != null && paneNext.Collapsed)
+                    )
+                    return false;
+
+
+                return Resizable;
+            }
+        }
+
+        internal bool IsCollapsible
+        {
+            get
+            {
+                if (Collapsible && !Collapsed)
+                    return true;
+
+                var paneNext = Next();
+                if (paneNext == null)
+                    return false;
+
+                return paneNext.IsLast && paneNext.Collapsible && paneNext.Collapsed;
+            }
+        }
+
+        internal bool IsExpandable
+        {
+            get
+            {
+                if (Collapsed)
+                    return true;
+
+                var paneNext = Next();
+                if (paneNext == null)
+                    return false;
+
+                return paneNext.IsLast && paneNext.Collapsible && !paneNext.Collapsed;
+            }
+        }
+
+        internal string ClassName
+        {
+            get
+            {
+                if (Collapsed)
+                    return "collapsed";
+
+                if (IsLastResizable)
+                    return "lastresizable";
+
+                if (IsResizable)
+                    return "resizable";
+
+                return "locked";
+            }
+        }
+    
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    
+        [Parameter]
+        public bool Collapsible { get; set; } = true;
+
+        [Parameter]
+        public bool Collapsed { get; set; }
+
+        [Parameter]
+        public bool Resizable { get; set; } = true;
+
+        [Parameter]
+        public string Max { get; set; }
+
+        [Parameter]
+        public string Min { get; set; }
+
+        [Parameter]
+        public string Size
+        {
+            get => SizeRuntine ?? _size;
+            set => _size = value;
+        }
+
+        [CascadingParameter]
+        public RadzenSplitter Splitter
+        {
+            get => _splitter;
+            set
+            {
+                if (_splitter != value)
+                {
+                    _splitter = value;
+                    _splitter.AddPane(this);
+                }
+            }
+        }
+    
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            Splitter?.RemovePane(this);
+        }
+
+        protected override string GetComponentCssClass()
+        {
+            return $"rz-splitter-pane rz-splitter-pane-{ClassName}";
+        }
+
+        protected string GetComponentBarCssClass()
+        {
+            return $"rz-splitter-bar rz-splitter-bar-{ClassName}";
+        }
+    }
+}
