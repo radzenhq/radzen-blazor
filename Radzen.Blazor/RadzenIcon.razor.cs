05ce9531d1ebdd6ca3ba54d7408d5f6e651daf87
Vladimir Enchev <vladimir.enchev@gmail.com>
2021-10-07 09:45:51 +0300

    RadzenIcon reworked with partial class

---
+++ Radzen.Blazor/RadzenIcon.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Radzen.Blazor
+{
+    public partial class RadzenIcon : RadzenComponent
+    {
+        [Parameter]
+        public string Icon { get; set; }
+
+        protected override string GetComponentCssClass()
+        {
+            return "rzi d-inline-flex justify-content-center align-items-center";
+        }
+    }
+}
