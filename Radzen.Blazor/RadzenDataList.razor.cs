bd145f8c3ad40c2cf07be8c1d2a49640a764fe0d
Vladimir Enchev <vladimir.enchev@gmail.com>
2021-10-07 09:09:08 +0300

    RadzenDataList reworked with partial class

---
+++ Radzen.Blazor/RadzenDataList.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Radzen.Blazor
+{
+    public partial class RadzenDataList<TItem> : PagedDataBoundComponent<TItem>
+    {
+        protected override string GetComponentCssClass()
+        {
+            return "rz-datalist-content";
+        }
+
+        [Parameter]
+        public bool WrapItems { get; set; }
+    }
+}
