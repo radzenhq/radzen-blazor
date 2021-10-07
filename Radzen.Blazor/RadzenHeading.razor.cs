dd91ece92aa6f0d4bf7afb832b82025e15c29a1f
Vladimir Enchev <vladimir.enchev@gmail.com>
2021-10-07 09:40:00 +0300

    RadzenHeading reworked with partial class

---
+++ Radzen.Blazor/RadzenHeading.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Radzen.Blazor
+{
+    public partial class RadzenHeading : RadzenComponent
+    {
+        [Parameter]
+        public string Text { get; set; }
+
+        [Parameter]
+        public string Size { get; set; } = "H1";
+
+        protected override string GetComponentCssClass()
+        {
+            return "rz-heading";
+        }
+    }
+}
