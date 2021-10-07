4549f271a2de821a36e5d74160f573bf37f185bc
Vladimir Enchev <vladimir.enchev@gmail.com>
2021-10-07 10:20:23 +0300

    RadzenProfileMenu reworked with partial class

---
+++ Radzen.Blazor/RadzenProfileMenu.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Radzen.Blazor
+{
+    public partial class RadzenProfileMenu : RadzenComponentWithChildren
+    {
+        protected override string GetComponentCssClass()
+        {
+            return "rz-menu rz-profile-menu";
+        }
+
+        [Parameter]
+        public RenderFragment Template { get; set; }
+
+        [Parameter]
+        public EventCallback<RadzenProfileMenuItem> Click { get; set; }
+
+        string contentStyle = "display:none;position:absolute;z-index:1;";
+        string iconStyle = "transform: rotate(0deg);";
+
+        public void Toggle(MouseEventArgs args)
+        {
+            contentStyle = contentStyle.IndexOf("display:none;") != -1 ? "display:block;" : "display:none;position:absolute;z-index:1;";
+            iconStyle = iconStyle.IndexOf("rotate(0deg)") != -1 ? "transform: rotate(-180deg);" : "transform: rotate(0deg);";
+            StateHasChanged();
+        }
+
+        public void Close()
+        {
+            contentStyle = "display:none;";
+            iconStyle = "transform: rotate(0deg);";
+            StateHasChanged();
+        }
+    }
+}
---
+++ Radzen.Blazor/RadzenProfileMenuItem.razor.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Radzen.Blazor
+{
+    public partial class RadzenProfileMenuItem : RadzenComponent
+    {
+        protected override string GetComponentCssClass()
+        {
+            return "rz-navigation-item";
+        }
+
+        [Parameter]
+        public string Target { get; set; }
+
+        [Parameter]
+        public string Path { get; set; }
+
+        [Parameter]
+        public string Icon { get; set; }
+
+        [Parameter]
+        public string Text { get; set; }
+
+        [Parameter]
+        public string Value { get; set; }
+
+        [CascadingParameter]
+        public RadzenProfileMenu Menu { get; set; }
+
+
+        public async System.Threading.Tasks.Task OnClick(MouseEventArgs args)
+        {
+            if (Menu != null)
+            {
+                Menu.Close();
+                await Menu.Click.InvokeAsync(this);
+            }
+        }
+    }
+}
