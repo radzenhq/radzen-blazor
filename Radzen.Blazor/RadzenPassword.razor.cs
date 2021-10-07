92c2b2566b6661875fb2a75eb300ee4c3d4fc338
Vladimir Enchev <vladimir.enchev@gmail.com>
2021-10-07 10:14:50 +0300

    RadzenPassword reworked with partial class

---
+++ Radzen.Blazor/RadzenPassword.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Radzen.Blazor
+{
+    public partial class RadzenPassword : FormComponent<string>, IRadzenFormComponent
+    {
+        [Parameter]
+        public bool ReadOnly { get; set; }
+
+        [Parameter]
+        public bool AutoComplete { get; set; } = true;
+
+        protected async System.Threading.Tasks.Task OnChange(ChangeEventArgs args)
+        {
+            Value = $"{args.Value}";
+
+            await ValueChanged.InvokeAsync(Value);
+            if (FieldIdentifier.FieldName != null) { EditContext?.NotifyFieldChanged(FieldIdentifier); }
+            await Change.InvokeAsync(Value);
+        }
+
+        protected override string GetComponentCssClass()
+        {
+            return GetClassList("rz-textbox").ToString();
+        }
+    }
+}
