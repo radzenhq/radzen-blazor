5f4cc07a988fe663678440ef0346a637759d6b3d
Vladimir Enchev <vladimir.enchev@gmail.com>
2021-10-07 09:20:12 +0300

    DropDown reworked with partial class

---
+++ Radzen.Blazor/RadzenDropDown.razor.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace Radzen.Blazor
+{
+    public partial class RadzenDropDown<TValue> : DropDownBase<TValue>
+    {
+        internal override void RenderItem(RenderTreeBuilder builder, object item)
+        {
+            builder.OpenComponent(0, typeof(RadzenDropDownItem<TValue>));
+            builder.AddAttribute(1, "DropDown", this);
+            builder.AddAttribute(2, "Item", item);
+            builder.SetKey(item);
+            builder.CloseComponent();
+        }
+
+        [Parameter]
+        public int MaxSelectedLabels { get; set; } = 4;
+
+        [Parameter]
+        public string SelectedItemsText { get; set; } = "items selected";
+
+        [Parameter]
+        public string SelectAllText { get; set; }
+
+        private bool visibleChanged = false;
+        private bool disabledChanged = false;
+        private bool firstRender = true;
+
+        public override async Task SetParametersAsync(ParameterView parameters)
+        {
+            visibleChanged = parameters.DidParameterChange(nameof(Visible), Visible);
+            disabledChanged = parameters.DidParameterChange(nameof(Disabled), Disabled);
+
+            await base.SetParametersAsync(parameters);
+
+            if (visibleChanged && !firstRender)
+            {
+                if (Visible == false)
+                {
+                    Dispose();
+                }
+            }
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+
+            this.firstRender = firstRender;
+
+            if (firstRender || visibleChanged || disabledChanged)
+            {
+                visibleChanged = false;
+                disabledChanged = false;
+
+                if (Visible)
+                {
+                    bool reload = false;
+                    if (LoadData.HasDelegate && Data == null)
+                    {
+                        await LoadData.InvokeAsync(await GetLoadDataArgs());
+                        reload = true;
+                    }
+
+                    if (!Disabled)
+                    {
+                        await JSRuntime.InvokeVoidAsync("Radzen.preventArrows", Element);
+                        reload = true;
+                    }
+
+                    if (reload)
+                    {
+                        StateHasChanged();
+                    }
+                }
+            }
+        }
+
+        protected override async System.Threading.Tasks.Task OnSelectItem(object item, bool isFromKey = false)
+        {
+            if (!Multiple && !isFromKey)
+            {
+                await JSRuntime.InvokeVoidAsync("Radzen.closePopup", PopupID);
+            }
+
+            await SelectItem(item);
+        }
+
+        internal async System.Threading.Tasks.Task OnSelectItemInternal(object item, bool isFromKey = false)
+        {
+            await OnSelectItem(item, isFromKey);
+        }
+
+        protected override string GetComponentCssClass()
+        {
+            return GetClassList("rz-dropdown").Add("rz-clear", AllowClear).ToString();
+        }
+
+        private string ClosePopupScript()
+        {
+            if (Disabled)
+            {
+                return string.Empty;
+            }
+
+            return $"Radzen.closePopup('{PopupID}')";
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+
+            if (IsJSRuntimeAvailable)
+            {
+                JSRuntime.InvokeVoidAsync("Radzen.destroyPopup", PopupID);
+            }
+        }
+
+        internal async System.Threading.Tasks.Task ClosePopup()
+        {
+            await JSRuntime.InvokeVoidAsync("Radzen.closePopup", PopupID);
+        }
+    }
+}
