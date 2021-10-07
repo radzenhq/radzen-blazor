ce345a3c716b25f0e474edddc51dcdaf432494d0
Vladimir Enchev <vladimir.enchev@gmail.com>
2021-10-07 09:32:15 +0300

    RadzenFooter reworked with partial class

---
+++ Radzen.Blazor/RadzenFooter.razor.cs
@@ -0,0 +1,10 @@
+﻿namespace Radzen.Blazor
+{
+    public partial class RadzenFooter : RadzenComponentWithChildren
+    {
+        protected override string GetComponentCssClass()
+        {
+            return "footer fixed";
+        }
+    }
+}
