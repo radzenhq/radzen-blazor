3f104982ab14c8ceeada3783a52c047c8ae6038e
Vladimir Enchev <vladimir.enchev@gmail.com>
2021-10-18 15:10:06 +0300

    ContextMenuService example added

--- Radzen.Blazor/ContextMenuService.cs
+++ Radzen.Blazor/ContextMenuService.cs
@@ -8,8 +8,33 @@ using System.Threading.Tasks;
 namespace Radzen
 {
     /// <summary>
-    /// Class ContextMenuService.
+    /// Class ContextMenuService. Contains variuos methods with options to open and close context menus. 
+    /// Should be added as scoped service in the application services and RadzenContextMenu should be added in application main layout.
     /// Implements the <see cref="IDisposable" />
+    /// <example>
+    /// <code>
+    /// @inject ContextMenuService ContextMenuService
+    /// &lt;RadzenButton Text="Show context menu" ContextMenu=@(args => ShowContextMenuWithContent(args)) /&gt;
+    /// @code {
+    ///     void ShowContextMenuWithContent(MouseEventArgs args) =&gt; ContextMenuService.Open(args, ds =&gt;
+    ///       @&lt;RadzenMenu Click="OnMenuItemClick"&gt;
+    ///         &lt;RadzenMenuItem Text="Item1" Value="1"&gt;&lt;/RadzenMenuItem&gt;
+    ///         &lt;RadzenMenuItem Text="Item2" Value="2"&gt;&lt;/RadzenMenuItem&gt;
+    ///         &lt;RadzenMenuItem Text="More items" Value="3"&gt;
+    ///             &lt;RadzenMenuItem Text="More sub items" Value="4"&gt;
+    ///                 &lt;RadzenMenuItem Text="Item1" Value="5"&gt;&lt;/RadzenMenuItem&gt;
+    ///                 &lt;RadzenMenuItem Text="Item2" Value="6"&gt;&lt;/RadzenMenuItem&gt;
+    ///             &lt;/RadzenMenuItem&gt;
+    ///         &lt;/RadzenMenuItem&gt;
+    ///       &lt;/RadzenMenu&gt;);
+    /// 
+    ///     void OnMenuItemClick(MenuItemEventArgs args)
+    ///     {
+    ///       Console.WriteLine($"Menu item with Value={args.Value} clicked");
+    ///     }
+    /// }
+    /// </code>
+    /// </example>
     /// </summary>
     /// <seealso cref="IDisposable" />
     public class ContextMenuService : IDisposable
