f3ef9224ef37d81bfa91cea9b30f912683c6ad4e
Vladimir Enchev <vladimir.enchev@gmail.com>
2021-10-19 09:44:19 +0300

    DatePicker will select wrong month when choosing date from previous/next month

--- Radzen.Blazor/RadzenDatePicker.razor.cs
+++ Radzen.Blazor/RadzenDatePicker.razor.cs
@@ -882,7 +882,7 @@ namespace Radzen.Blazor
         {
             if (ShowTimeOkButton)
             {
-                CurrentDate = new DateTime(CurrentDate.Year, CurrentDate.Month,newValue.Day, CurrentDate.Hour, CurrentDate.Minute, CurrentDate.Second);
+                CurrentDate = new DateTime(CurrentDate.Year, newValue.Month,newValue.Day, CurrentDate.Hour, CurrentDate.Minute, CurrentDate.Second);
                 await OkClick();
             }
             else
