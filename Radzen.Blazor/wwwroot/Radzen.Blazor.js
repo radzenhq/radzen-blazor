6af95ab3df03016a75e7a0a6cc3f34c499984906
David <david.alonso.su@gmail.com>
2022-02-19 18:56:32 +0100

    RadzenFileInput: Add MaxWith And MaxHeight to resize FileInput Image.

--- Radzen.Blazor/wwwroot/Radzen.Blazor.js
+++ Radzen.Blazor/wwwroot/Radzen.Blazor.js
@@ -910,7 +910,26 @@ window.Radzen = {
       input.value = value;
     }
   },
-  readFileAsBase64: function (fileInput, maxFileSize) {
+  readFileAsBase64: function (fileInput, maxFileSize, maxWidth, maxHeight) {
+      var calculateWidthAndHeight = function (img, maxWidthImg, maxHeightImg) {
+        var width = img.width;
+        var height = img.height;
+
+        // Change the resizing logic
+        if (width > height) {
+            if (width > maxWidthImg) {
+                height = height * (maxWidthImg / width);
+                width = maxWidthImg;
+            }
+        } else {
+            if (height > maxHeightImg) {
+                width = width * (maxHeightImg / height);
+                height = maxHeightImg;
+            }
+          }
+          return { width, height };
+    };
+
     var readAsDataURL = function (fileInput) {
       return new Promise(function (resolve, reject) {
         var reader = new FileReader();
@@ -921,10 +940,24 @@ window.Radzen = {
         reader.addEventListener(
           'load',
           function () {
-            resolve(reader.result);
+              if (maxWidth > 0 && maxHeight > 0) {
+                  var img = document.createElement("img");
+                  img.onload = function (event) {
+                      // Dynamically create a canvas element
+                      var canvas = document.createElement("canvas");
+                      var ctx = canvas.getContext("2d");
+                      var res = calculateWidthAndHeight(img, maxWidth, maxHeight);
+                      ctx.drawImage(img, 0, 0, res.width, res.height);
+                      resolve(canvas.toDataURL(fileInput.type));
+                  }
+                  img.src = reader.result;
+                 
+              } else {
+                  resolve(reader.result);
+              }
           },
           false
-        );
+          );
         var file = fileInput.files[0];
         if (!file) return;
         if (file.size <= maxFileSize) {
