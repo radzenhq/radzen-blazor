607df67f0309dd2628f24939356d49fdd3926dcc
Marco Papst <marco@papst.io>
2022-03-02 22:57:40 +0100

    add BreadCrumb Component

---
+++ Radzen.Blazor/RadzenBreadCrumb.razor.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Radzen.Blazor
+{
+    /// <summary>
+    /// A component to display a Bread Crumb style menu
+    /// </summary>
+    /// <typeparam name="TItem"></typeparam>
+    public partial class RadzenBreadCrumb<TItem> : RadzenComponent
+    {
+        private int _count;
+
+        /// <summary>
+        /// An optional RenderFragment that is rendered per Item
+        /// in the <see cref="Data"/> Collection
+        /// </summary>
+        [Parameter]
+        public RenderFragment<TItem> Template { get; set; }
+
+        /// <summary>
+        /// An optional RenderFragment that is rendered between Items
+        /// </summary>
+        [Parameter]
+        public RenderFragment SeparatorTemplate { get; set; }
+
+        /// <summary>
+        /// A Separator string that is rendered between Items
+        /// if <see cref="SeparatorTemplate"/> is not set
+        /// </summary>
+        [Parameter]
+        public string Separator { get; set; } = "»";
+
+        /// <summary>
+        /// The name of the property used to get the content
+        /// </summary>
+        [Parameter]
+        public string TextProperty { get; set; }
+
+        /// <summary>
+        /// Optional name of the property holding a path or link
+        /// </summary>
+        [Parameter]
+        public string LinkProperty { get; set; }
+
+        /// <summary>
+        /// The Items to be displayed
+        /// </summary>
+        [Parameter]
+        public IEnumerable<TItem> Data { get; set; }
+
+        /// <inheritdoc/>
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+            // temporary store the item count on each set
+            _count = Data?.Count() ?? 0;
+        }
+    }
+}
---
+++ Radzen.Blazor/RadzenBreadCrumbItem.razor.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace Radzen.Blazor
+{
+    /// <summary>
+    /// Bread Crumb Item Component
+    /// </summary>
+    /// <typeparam name="TItem"></typeparam>
+    public partial class RadzenBreadCrumbItem<TItem> : RadzenComponent
+    {
+        /// <summary>
+        /// Parent <see cref="RadzenBreadCrumb{TItem}"/>
+        /// </summary>
+        [CascadingParameter]
+        public RadzenBreadCrumb<TItem> BreadCrumb { get; set; }
+
+        /// <summary>
+        /// The Item
+        /// </summary>
+        [Parameter]
+        public TItem Item { get; set; }
+
+        private readonly Type type = typeof(TItem);
+
+        private string GetLink()
+        {
+            if (BreadCrumb.LinkProperty == null)
+            {
+                return string.Empty;
+            }
+            else
+            {
+                return type
+                    .GetProperty(BreadCrumb.LinkProperty)
+                    .GetValue(Item, null)
+                    .ToString();
+            }
+        }
+
+        private string GetText()
+        {
+            if (BreadCrumb.TextProperty == null)
+            {
+                return string.Empty;
+            }
+            else
+            {
+                return type
+                    .GetProperty(BreadCrumb.TextProperty)
+                    .GetValue(Item, null)
+                    .ToString();
+            }
+        }
+    }
+}
