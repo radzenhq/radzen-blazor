f1f7124e3522a3c7b6b99356b5322454218c2749
Benjamin Vertonghen <benjamin.vertonghen@hogent.be>
2022-05-11 15:07:11 +0200

    Add Display Attribute and localization

---
+++ Radzen.Blazor/Extensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace Radzen.Blazor
+{
+    public static class EnumExtensions
+    {
+        public static string GetDisplayDescription(this Enum enumValue)
+        {
+            var val = enumValue.GetType().GetMember(enumValue.ToString()).FirstOrDefault();
+
+            if (val is null)
+                return enumValue.ToString();
+
+            var attribute = val?.GetCustomAttribute<DisplayAttribute>();
+            if (attribute != null)
+                return attribute?.Description;
+
+            return enumValue.ToString();
+        }
+
+        public static IEnumerable<object> EnumAsKeyValuePair(Type enumType)
+        {
+            var values = Enum.GetValues(enumType);
+            var items = new List<object>(values.Length);
+
+            foreach (Enum val in values)
+            {
+                items.Add(new { Value = val, Text = val.GetDisplayDescription() });
+            }
+
+            return items;
+        }
+
+    }
+}
+
