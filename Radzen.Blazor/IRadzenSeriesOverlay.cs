58ce4fc61271d953695162ac741b2b998b1dd35d
glutio <evpistra@microsoft.com>
2022-07-04 12:56:37 -0700

    Constant value line overlay

---
+++ Radzen.Blazor/IRadzenSeriesOverlay.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Radzen.Blazor
+{
+    internal interface IRadzenSeriesOverlay
+    {
+        RenderFragment Render(ScaleBase categoryScale, ScaleBase valueScale);
+    }
+}
