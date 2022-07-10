bbfb87971d1f10d453af2d1602fb51b6d53a04ab
glutio <evpistra@microsoft.com>
2022-07-09 17:48:18 -0700

    Added BusyLoadingService and a Service parameter on RadzenBusyLoading component to allow indirect control

---
+++ Radzen.Blazor/BusyLoadingService.cs
@@ -0,0 +1,22 @@
+﻿namespace Radzen.Blazor
+{
+    /// <summary>
+    /// A service to control a RadzenBusyLoading component
+    /// </summary>
+    public class BusyLoadingService
+    {
+        internal delegate void StateChanged(bool val);
+        internal event StateChanged BusyChanged;
+        internal event StateChanged LoadedChanged;
+
+        public void Busy(bool val)
+        {
+            BusyChanged.Invoke(val);
+        }
+
+        public void Loaded(bool val)
+        {
+            BusyChanged.Invoke(val);
+        }
+    }
+}
