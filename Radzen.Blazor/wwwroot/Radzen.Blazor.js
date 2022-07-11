d1a7218c1b0550330b8927c8144c6db75f12e94d
Solovey Alexandr <solovey@rgroup-cargo.com>
2022-07-11 15:48:30 +0300

    Revert "fixed for long map loading"
    This reverts commit 900d69f6adeab7e3bf1594f30a432295ab692f92.

--- Radzen.Blazor/wwwroot/Radzen.Blazor.js
+++ Radzen.Blazor/wwwroot/Radzen.Blazor.js
@@ -230,13 +230,11 @@ window.Radzen = {
     });
   },
   customizeMap: function(id, draggingCursor) {
-      if (Radzen[id] && Radzen[id].instance) {
-          Radzen[id].instance.addListener('tilesloaded', function () {
-              Radzen[id].instance.setOptions({
-                  draggableCursor: draggingCursor
-              });
-          });
-      }
+    if (Radzen[id] && Radzen[id].instance) {
+      Radzen[id].instance.setOptions({
+         draggableCursor: draggingCursor
+        });
+    }
   },
   updateMap: function (id, zoom, center, markers) {
     if (Radzen[id] && Radzen[id].instance) {
