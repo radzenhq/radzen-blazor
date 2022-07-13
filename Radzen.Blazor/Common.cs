6befc88dd3e1daa104e2f5cd90965ba627fec7ce
Atanas Korchev <akorchev@gmail.com>
2022-07-13 19:38:52 +0300

    Add RadzenText.

--- Radzen.Blazor/Common.cs
+++ Radzen.Blazor/Common.cs
@@ -869,7 +869,15 @@ namespace Radzen
         /// <summary>
         /// The text is centered in its container.
         /// </summary>
-        Center
+        Center,
+        /// <summary>The text is justified.</summary>
+        Justify,
+        /// <summary>Same as justify, but also forces the last line to be justified.</summary>
+        JustifyAll,
+        /// <summary>The same as left if direction is left-to-right and right if direction is right-to-left..</summary>
+        Start,
+        /// <summary>The same as right if direction is left-to-right and left if direction is right-to-left..</summary>
+        End
     }
 
     /// <summary>
