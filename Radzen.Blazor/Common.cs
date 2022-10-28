64165a868f420c90ecdb61879303ab8799091a5a
yordanov <vasil@yordanov.info>
2022-10-28 18:31:47 +0300

    Add AlertSize and update scss

--- Radzen.Blazor/Common.cs
+++ Radzen.Blazor/Common.cs
@@ -732,6 +732,29 @@ namespace Radzen
     }
 
     /// <summary>
+    /// Specifies the size of a <see cref="RadzenAlert" />.
+    /// </summary>
+    public enum AlertSize
+    {
+        /// <summary>
+        /// The smallest alert.
+        /// </summary>
+        ExtraSmall,
+        /// <summary>
+        /// A alert smaller than the default.
+        /// </summary>
+        Small,
+        /// <summary>
+        /// The default size of an alert.
+        /// </summary>
+        Medium,
+        /// <summary>
+        /// An alert larger than the default.
+        /// </summary>
+        Large
+    }
+
+    /// <summary>
     /// Specifies the behavior of <see cref="RadzenProgressBar" />.
     /// </summary>
     public enum ProgressBarMode
