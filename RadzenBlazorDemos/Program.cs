1d98c49f354c2ad582fbbda132fd58dd635ffce4
Atanas Korchev <akorchev@gmail.com>
2022-11-11 13:32:25 +0200

    Remove startup.

--- RadzenBlazorDemos/Program.cs
+++ RadzenBlazorDemos/Program.cs
@@ -1,38 +1,29 @@
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using RadzenBlazorDemos.Services;
 using System;
 using System.Net.Http;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+using RadzenBlazorDemos.Services;
 using Radzen;
 using RadzenBlazorDemos.Data;
+using RadzenBlazorDemos;
 
-namespace RadzenBlazorDemos
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            builder.Services.AddDbContextFactory<NorthwindContext>();
+builder.Services.AddDbContextFactory<NorthwindContext>();
 
-            builder.Services.AddScoped<ThemeService>();
-            builder.Services.AddScoped<ExampleService>();
-            builder.Services.AddScoped<DialogService>();
-            builder.Services.AddScoped<NotificationService>();
-            builder.Services.AddScoped<TooltipService>();
-            builder.Services.AddScoped<ContextMenuService>();
+builder.Services.AddScoped<ThemeService>();
+builder.Services.AddScoped<ExampleService>();
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ContextMenuService>();
 
-            builder.Services.AddScoped<NorthwindService>();
-            builder.Services.AddScoped<NorthwindODataService>();
-            builder.Services.AddScoped<GitHubService>();
+builder.Services.AddScoped<NorthwindService>();
+builder.Services.AddScoped<NorthwindODataService>();
+builder.Services.AddScoped<GitHubService>();
 
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+await builder.Build().RunAsync();
