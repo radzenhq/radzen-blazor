a5c522733aa2893cc6f2ad0ca829c460c01c89e7
Maxim Becker <becker.maxim@gmail.com>
2022-11-28 16:16:35 +0100

    Add interpolation in RadzenLineSeries to make rendering of them more generic

---
+++ Radzen.Blazor/Interpolation.cs
@@ -0,0 +1,12 @@
+﻿namespace Radzen.Blazor
+{
+    /// <summary>
+    /// Specifies the interpolation mode of lines between data points
+    /// </summary>
+    public enum Interpolation
+    {
+        Line,
+        Spline,
+        Step
+    }
+}
