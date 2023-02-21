e502c51886b258ce31f503fd54a765af96783239
Artur Madjidov <36833825+Arturace@users.noreply.github.com>
2023-02-21 05:15:40 -0500

    Add parameters for the example texts in RadzenHtmlEditorFormatBlock. (#821)

--- Radzen.Blazor/RadzenHtmlEditorFormatBlock.razor.cs
+++ Radzen.Blazor/RadzenHtmlEditorFormatBlock.razor.cs
@@ -36,6 +36,48 @@ namespace Radzen.Blazor
         [Parameter]
         public string Title { get; set; } = "Text style";
 
+        /// <summary>
+        /// Specifies the text displayed for the normal text example. Set to <c>"Normal"</c> by default.
+        /// </summary>
+        [Parameter]
+        public string NormalText { get; set; } = "Normal";
+
+        /// <summary>
+        /// Specifies the text displayed for the h1 example. Set to <c>"Heading 1"</c> by default.
+        /// </summary>
+        [Parameter]
+        public string Heading1Text { get; set; } = "Heading 1";
+
+        /// <summary>
+        /// Specifies the text displayed for the h2 example. Set to <c>"Heading 2"</c> by default.
+        /// </summary>
+        [Parameter]
+        public string Heading2Text { get; set; } = "Heading 2";
+
+        /// <summary>
+        /// Specifies the text displayed for the h3 example. Set to <c>"Heading 3"</c> by default.
+        /// </summary>
+        [Parameter]
+        public string Heading3Text { get; set; } = "Heading 3";
+
+        /// <summary>
+        /// Specifies the text displayed for the h4 example. Set to <c>"Heading 4"</c> by default.
+        /// </summary>
+        [Parameter]
+        public string Heading4Text { get; set; } = "Heading 4";
+
+        /// <summary>
+        /// Specifies the text displayed for the h5 example. Set to <c>"Heading 5"</c> by default.
+        /// </summary>
+        [Parameter]
+        public string Heading5Text { get; set; } = "Heading 5";
+
+        /// <summary>
+        /// Specifies the text displayed for the h6 example. Set to <c>"Heading 6"</c> by default.
+        /// </summary>
+        [Parameter]
+        public string Heading6Text { get; set; } = "Heading 6";
+
         async Task OnChange(string value)
         {
             await Editor.ExecuteCommandAsync("formatBlock", value);
