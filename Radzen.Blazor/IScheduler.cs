0720a9b8f0b9c4488a96c811ed53cc0ca820a4f7
Paul Ruston <60648319+paulo-rico@users.noreply.github.com>
2023-03-31 07:11:05 +0100

    Scheduler changes - Better handling of Yearview clicks and overridable MoreText event (#874)
    * React to clicks in YearView better and create SelectMore Event to override default code.
    
    * Remove unused method
    
    * Revert "Remove unused method"
    
    This reverts commit 646542c7bf7b388528dff5994befddea8a6d1195.
    
    * Revert "React to clicks in YearView better and create SelectMore Event to override default code."
    
    This reverts commit 0f7bab53009687f98acb2d3d367bf4853c839325.
    
    * Scheduler DaySelect event. Also cleaned up some code (removed nested if...else...)
    
    * Revert "Scheduler DaySelect event. Also cleaned up some code (removed nested if...else...)"
    
    This reverts commit 1d15a8fa069ffb624fcae297f909d93213b074d6.
    
    * Extend SlotSelect to pass Appointments. Bit of code condense in YearView
    
    * Added original SelectSlot method signature to IScheduler and RadzenScheduler
    
    * Ignore this push
    
    * Revert tabs in MonthView
    
    * Revert "Revert tabs in MonthView"
    
    This reverts commit d65a6ec49030359c344861831bb4ee0bc64b08df.
    
    * Revert "Ignore this push"
    
    This reverts commit 0c5fae70ed4bcf6bd4db28b5fc171fd8ba94c22f.
    
    * Revert "Added original SelectSlot method signature to IScheduler and RadzenScheduler"
    
    This reverts commit 320b353af6cd030e7a7917058e3858aac7a5bd8f.
    
    * Revert "Extend SlotSelect to pass Appointments. Bit of code condense in YearView"
    
    This reverts commit e7cbebedc88131f31c0b2626e1ba6f2be67e237f.
    
    * Extend SelectSlot to include list of appointments in slot
    
    * Revert "Extend SelectSlot to include list of appointments in slot"
    
    This reverts commit c901ec3b48b042a63d756259a0805eb46b85e29a.
    
    * Implement ability to prevent default action on YearView ListClick. Add MoreSelect and offer same default prevention for Month, Planner and Timeline views. Extend SlotArgs to include list of appointments and current view.
    
    * Do not allow add and edit in Year view.
    
    ---------
    
    Co-authored-by: Paul Ruston <paul.ruston@live.co.uk>
    Co-authored-by: Atanas Korchev <akorchev@gmail.com>

--- Radzen.Blazor/IScheduler.cs
+++ Radzen.Blazor/IScheduler.cs
@@ -59,6 +59,20 @@ namespace Radzen.Blazor
         /// <param name="end">The end.</param>
         Task SelectSlot(DateTime start, DateTime end);
         /// <summary>
+        /// Selects the specified slot.
+        /// </summary>
+        /// <param name="start">The start.</param>
+        /// <param name="end">The end.</param>
+        /// <param name="appointments">The appointments for this range.</param>
+        Task<bool> SelectSlot(DateTime start, DateTime end, IEnumerable<AppointmentData> appointments);
+        /// <summary>
+        /// Selects the specified more link.
+        /// </summary>
+        /// <param name="start">The start.</param>
+        /// <param name="end">The end.</param>
+        /// <param name="appointments">The appointments for this range.</param>
+        Task<bool> SelectMore(DateTime start, DateTime end, IEnumerable<AppointmentData> appointments);
+        /// <summary>
         /// Gets the appointment HTML attributes.
         /// </summary>
         /// <param name="item">The appointment.</param>
