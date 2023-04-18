8587bdec899323198ca9e9444aa7c696030d74b5
ml <m.l.2021@yandex.ru>
2023-04-18 09:44:40 +0700

    Feature: add readonly behavior for RadzenCheckBox

--- Radzen.Blazor/RadzenCheckBox.razor.cs
+++ Radzen.Blazor/RadzenCheckBox.razor.cs
@@ -7,87 +7,87 @@ using System.Threading.Tasks;
 
 namespace Radzen.Blazor
 {
-  /// <summary>
-  /// RadzenCheckBox component.
-  /// </summary>
-  /// <typeparam name="TValue">The type of the value.</typeparam>
-  /// <example>
-  /// <code>
-  /// &lt;RadzenCheckBox @bind-Value=@someValue TValue="bool" Change=@(args => Console.WriteLine($"Is checked: {args}")) /&gt;
-  /// </code>
-  /// </example>
-  public partial class RadzenCheckBox<TValue> : FormComponent<TValue>
-  {
     /// <summary>
-    /// Gets or sets a value indicating whether is tri-state (true, false or null).
+    /// RadzenCheckBox component.
     /// </summary>
-    /// <value><c>true</c> if tri-state; otherwise, <c>false</c>.</value>
-    [Parameter]
-    public bool TriState { get; set; } = false;
+    /// <typeparam name="TValue">The type of the value.</typeparam>
+    /// <example>
+    /// <code>
+    /// &lt;RadzenCheckBox @bind-Value=@someValue TValue="bool" Change=@(args => Console.WriteLine($"Is checked: {args}")) /&gt;
+    /// </code>
+    /// </example>
+    public partial class RadzenCheckBox<TValue> : FormComponent<TValue>
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether is tri-state (true, false or null).
+        /// </summary>
+        /// <value><c>true</c> if tri-state; otherwise, <c>false</c>.</value>
+        [Parameter]
+        public bool TriState { get; set; } = false;
 
-    /// <summary>
-    /// Gets or sets a value indicating whether read only.
-    /// </summary>
-    /// <value><c>true</c> if read only; otherwise, <c>false</c>.</value>
-    [Parameter]
-    public bool ReadOnly { get; set; }
+        /// <summary>
+        /// Gets or sets a value indicating whether read only.
+        /// </summary>
+        /// <value><c>true</c> if read only; otherwise, <c>false</c>.</value>
+        [Parameter]
+        public bool ReadOnly { get; set; }
 
-    ClassList BoxClassList => ClassList.Create("rz-chkbox-box")
+        ClassList BoxClassList => ClassList.Create("rz-chkbox-box")
                                            .Add("rz-state-active", !object.Equals(Value, false))
                                            .AddDisabled(Disabled);
 
-    ClassList IconClassList => ClassList.Create("rz-chkbox-icon")
-                                        .Add("rzi rzi-check", object.Equals(Value, true))
-                                        .Add("rzi rzi-times", object.Equals(Value, null));
+        ClassList IconClassList => ClassList.Create("rz-chkbox-icon")
+                                            .Add("rzi rzi-check", object.Equals(Value, true))
+                                            .Add("rzi rzi-times", object.Equals(Value, null));
 
-    string CheckBoxValue => CheckBoxChecked ? "true" : "false";
+        string CheckBoxValue => CheckBoxChecked ? "true" : "false";
 
-    bool CheckBoxChecked => object.Equals(Value, true);
+        bool CheckBoxChecked => object.Equals(Value, true);
 
-    /// <inheritdoc />
-    protected override string GetComponentCssClass()
-    {
-      return GetClassList("rz-chkbox").ToString();
-    }
-
-    async Task OnKeyPress(KeyboardEventArgs args)
-    {
-      if (args.Code == "Space")
-      {
-        await Toggle();
-      }
-    }
-
-    async Task Toggle()
-    {
-      if (Disabled || ReadOnly)
-      {
-        return;
-      }
-
-      if (object.Equals(Value, false))
-      {
-        if (TriState)
+        /// <inheritdoc />
+        protected override string GetComponentCssClass()
         {
-          Value = default;
+            return GetClassList("rz-chkbox").ToString();
         }
-        else
-        {
-          Value = (TValue)(object)true;
-        }
-      }
-      else if (Value == null)
-      {
-        Value = (TValue)(object)true;
-      }
-      else if (object.Equals(Value, true))
-      {
-        Value = (TValue)(object)false;
-      }
 
-      await ValueChanged.InvokeAsync(Value);
-      if (FieldIdentifier.FieldName != null) { EditContext?.NotifyFieldChanged(FieldIdentifier); }
-      await Change.InvokeAsync(Value);
+        async Task OnKeyPress(KeyboardEventArgs args)
+        {
+            if (args.Code == "Space")
+            {
+                await Toggle();
+            }
+        }
+
+        async Task Toggle()
+        {
+            if (Disabled || ReadOnly)
+            {
+                return;
+            }
+
+            if (object.Equals(Value, false))
+            {
+                if (TriState)
+                {
+                    Value = default;
+                }
+                else
+                {
+                    Value = (TValue)(object)true;
+                }
+            }
+            else if (Value == null)
+            {
+                Value = (TValue)(object)true;
+            }
+            else if (object.Equals(Value, true))
+            {
+                Value = (TValue)(object)false;
+            }
+
+            await ValueChanged.InvokeAsync(Value);
+            if (FieldIdentifier.FieldName != null) { EditContext?.NotifyFieldChanged(FieldIdentifier); }
+            await Change.InvokeAsync(Value);
+        }
     }
-  }
 }
