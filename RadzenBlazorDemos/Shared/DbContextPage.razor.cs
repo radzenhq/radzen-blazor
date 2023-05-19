edf381c9142e136acaa337ed42a7a7e8f8be8e22
Atanas Korchev <akorchev@gmail.com>
2023-05-19 11:55:03 +0300

    Address issues introduced by .NET 7.0.302.

---
+++ RadzenBlazorDemos/Shared/DbContextPage.razor.cs
@@ -0,0 +1,27 @@
+﻿using RadzenBlazorDemos.Data;
+using RadzenBlazorDemos.Models.Northwind;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace RadzenBlazorDemos.Shared
+{
+    public partial class DbContextPage : ComponentBase
+    {
+        [Inject]
+        public IDbContextFactory<NorthwindContext> DbFactory { get; set; }
+
+        protected NorthwindContext dbContext;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+
+            if (dbContext == null)
+            {
+                dbContext = await DbFactory.CreateDbContextAsync();
+                await dbContext.SeedAsync();
+            }
+        }
+    }
+}
