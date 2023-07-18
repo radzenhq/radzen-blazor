a2209e73fc42d6cbbffc1b703b75788db7fa1573
Dave Bagler <dave@Baglerit.com>
2023-07-18 02:38:08 -0400

    Htmledit on input (#1070)
    * adding a paramter for allow for no-tool HTMLEditors.
    
    * Adding input event to the HTMLEditor

--- Radzen.Blazor/RadzenHtmlEditor.razor.cs
+++ Radzen.Blazor/RadzenHtmlEditor.razor.cs
@@ -19,6 +19,12 @@ namespace Radzen.Blazor
     public partial class RadzenHtmlEditor : FormComponent<string>
     {
         /// <summary>
+        /// Allows for no-tool HTML editors.
+        /// </summary>
+        [Parameter]
+        public bool NoTools { get; set; } = false;
+
+        /// <summary>
         /// Gets or sets the mode of the editor.
         /// </summary>
         [Parameter]
@@ -38,6 +44,13 @@ namespace Radzen.Blazor
         /// </summary>
         [Parameter]
         public IDictionary<string, string> UploadHeaders { get; set; }
+
+        /// <summary>
+        /// Gets or sets the input.
+        /// </summary>
+        /// <value>The input.</value>
+        [Parameter]
+        public EventCallback<string> Input { get; set; }
 
         /// <summary>
         /// A callback that will be invoked when the user pastes content in the editor. Commonly used to filter unwanted HTML.
@@ -274,6 +287,7 @@ namespace Radzen.Blazor
         public void OnChange(string html)
         {
             Html = html;
+            Input.InvokeAsync(html);
         }
 
         /// <summary>
