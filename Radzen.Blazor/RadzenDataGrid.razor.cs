b1d1d2c7f81c59ca4b30e4aa1cd26e038b46d08f
Vladimir Enchev <vladimir.enchev@gmail.com>
2023-08-29 09:45:00 +0300

    DataGrid will not call LoadData in simple filter with virtualization enabled

--- Radzen.Blazor/RadzenDataGrid.razor.cs
+++ Radzen.Blazor/RadzenDataGrid.razor.cs
@@ -326,7 +326,9 @@ namespace Radzen.Blazor
             var inputValue = await JSRuntime.InvokeAsync<string>("Radzen.getInputValue", getFilterInputId(column));
             if (!object.Equals(column.GetFilterValue(), inputValue))
             {
-                await OnFilter(new ChangeEventArgs() { Value = inputValue }, column);
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                await InvokeAsync(() => { OnFilter(new ChangeEventArgs() { Value = inputValue }, column); });
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             }
         }
 
