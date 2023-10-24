2205040ddca0b8fc501d0aa8fd3e7012e92897cd
Marek Mička <xmicka13@vutbr.cz>
2023-10-24 16:23:36 +0200

    Simple demo

---
+++ RadzenBlazorDemos/Models/UserInput.cs
@@ -0,0 +1,8 @@
+﻿namespace RadzenBlazorDemos.Models;
+
+public class UserInput
+{
+    public string FirstName { get; set; } = null;
+    public string Surename { get; set; } = null;
+    public string Email { get; set; }
+}
