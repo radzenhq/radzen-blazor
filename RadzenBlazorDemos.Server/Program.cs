03f992bfc54f24378f6cf2c779696f2a96ebebf8
Vladimir Enchev <vladimir.enchev@gmail.com>
2023-11-14 10:38:25 +0200

    various warnings fixed

--- RadzenBlazorDemos.Server/Program.cs
+++ RadzenBlazorDemos.Server/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddSingleton(sp =>
     // Get the address that the app is currently running at
     var server = sp.GetRequiredService<IServer>();
     var addressFeature = server.Features.Get<IServerAddressesFeature>();
-    string baseAddress = addressFeature.Addresses.First();
+    string baseAddress = addressFeature != null ? addressFeature.Addresses.First() : string.Empty;
     return new HttpClient { BaseAddress = new Uri(baseAddress) };
 });
 
