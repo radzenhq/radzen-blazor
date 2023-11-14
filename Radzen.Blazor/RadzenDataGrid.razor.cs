a17226744ceaf9c4292be4d1ae829fc68cb7dc48
Siebe Jan de Vries <siebejan.devries@axba.nl>
2023-11-14 11:40:19 +0100

    The EditRow doesn't use the same compare method as the CancelEditRow.

--- Radzen.Blazor/RadzenDataGrid.razor.cs
+++ Radzen.Blazor/RadzenDataGrid.razor.cs
@@ -2293,7 +2293,7 @@ namespace Radzen.Blazor
         /// <param name="item">The item.</param>
         public void CancelEditRow(TItem item)
         {
-            if (object.Equals(itemToInsert, item))
+            if (ItemEquals(itemToInsert, item))
             {
                 if(!IsVirtualizationAllowed())
                 {
