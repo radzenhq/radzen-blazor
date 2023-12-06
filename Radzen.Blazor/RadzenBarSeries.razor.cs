07295350c6f4949f47fc4707d67c8a821f14dc81
Paul Ruston <60648319+paulo-rico@users.noreply.github.com>
2023-12-06 12:42:50 +0000

    Add new series property 'ColorMode'.

--- Radzen.Blazor/RadzenBarSeries.razor.cs
+++ Radzen.Blazor/RadzenBarSeries.razor.cs
@@ -54,6 +54,13 @@ namespace Radzen.Blazor
         [Parameter]
         public LineType LineType { get; set; }
 
+        /// <summary>
+        /// Gets or sets the color mode of the series.
+        /// </summary>
+        /// <value>The color mode of the series.</value>
+        [Parameter]
+        public ColorMode ColorMode { get; set; } = ColorMode.Series;
+
         /// <inheritdoc />
         public override string Color
         {
@@ -102,16 +109,29 @@ namespace Radzen.Blazor
         {
             var style = base.TooltipStyle(item);
 
-            var index = Items.IndexOf(item);
-
-            if (index >= 0)
+            if (ColorMode == ColorMode.Series)
             {
-                var color = PickColor(index, Fills, Fill);
+                var index = Items.IndexOf(item);
+
+                if (index >= 0)
+                {
+                    var color = PickColor(index, Fills, Fill);
+
+                    if (color != null)
+                    {
+                        style = $"{style}; border-color: {color};";
+                    }
+                }
+            }
+            else
+            {
+                var color = PickColor(Value(item) < 0 ? 0 : 1, Fills, Fill);
 
                 if (color != null)
                 {
                     style = $"{style}; border-color: {color};";
                 }
+
             }
 
             return style;
