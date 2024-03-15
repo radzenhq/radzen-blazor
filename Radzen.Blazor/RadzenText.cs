10023477fe16b0d2e2ebd6b3bcaf74d28ea69850
yordanov <vasil@yordanov.info>
2024-03-15 10:22:35 +0200

    Revert TextAlign classes in RadzenText

--- Radzen.Blazor/RadzenText.cs
+++ Radzen.Blazor/RadzenText.cs
@@ -341,7 +341,7 @@ namespace Radzen.Blazor
 
             var classList = ClassList.Create(className)
                                      .Add(Attributes)
-                                     .Add(alignClassName);
+                                     .Add(alignClassName, TextAlign != TextAlign.Left);
 
             if (Visible)
             {
