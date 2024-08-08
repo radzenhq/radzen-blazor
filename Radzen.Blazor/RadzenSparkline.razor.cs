7cf36d70d802d0e77f8d8922b4041af47fdbc999
Atanas Korchev <akorchev@gmail.com>
2024-08-08 13:47:52 +0300

    Initial sparkline implementation.

---
+++ Radzen.Blazor/RadzenSparkline.razor.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Linq;
+
+namespace Radzen.Blazor
+{
+    /// <summary>
+    /// A sparkline is a small chart that provides a simple way to visualize trends in data.
+    /// </summary>
+    public partial class RadzenSparkline : RadzenChart
+    {
+        /// <summary>
+        /// Instantiates a new instance of <see cref="RadzenSparkline" />.
+        /// </summary>
+        public RadzenSparkline()
+        {
+            CategoryAxis = new RadzenCategoryAxis { Visible = false };
+            ValueAxis = new RadzenValueAxis { Visible = false };
+            Legend = new RadzenLegend { Visible = false };
+            TooltipTolerance = 5;
+        }
+
+        /// <summary>
+        /// Updates the scales based on the configuration.
+        /// </summary>
+        /// <returns>True if the chart should refresh; otherwise false;</returns>
+        protected override bool UpdateScales()
+        {
+            var valueScale = ValueScale;
+            var categoryScale = CategoryScale;
+
+            CategoryScale = new LinearScale { Output = CategoryScale.Output };
+            ValueScale = new LinearScale { Output = ValueScale.Output };
+
+            var visibleSeries = Series.Where(series => series.Visible).ToList();
+            var invisibleSeries = Series.Where(series => series.Visible == false).ToList();
+
+            if (!visibleSeries.Any() && invisibleSeries.Any())
+            {
+                visibleSeries.Add(invisibleSeries.Last());
+            }
+
+            foreach (var series in visibleSeries)
+            {
+                CategoryScale = series.TransformCategoryScale(CategoryScale);
+                ValueScale = series.TransformValueScale(ValueScale);
+            }
+
+            AxisBase xAxis = CategoryAxis;
+            AxisBase yAxis = ValueAxis;
+
+            if (ShouldInvertAxes())
+            {
+                xAxis = ValueAxis;
+                yAxis = CategoryAxis;
+            }
+            else
+            {
+                CategoryScale.Padding = CategoryAxis.Padding;
+            }
+
+            CategoryScale.Resize(xAxis.Min, xAxis.Max);
+
+            if (xAxis.Step != null)
+            {
+                CategoryScale.Step = xAxis.Step;
+                CategoryScale.Round = false;
+            }
+
+            ValueScale.Resize(yAxis.Min, yAxis.Max);
+
+            if (yAxis.Step != null)
+            {
+                ValueScale.Step = yAxis.Step;
+                ValueScale.Round = false;
+            }
+
+            MarginTop = MarginRight = MarginLeft = MarginBottom = 0;
+
+            CategoryScale.Output = new ScaleRange { Start = MarginLeft, End = Width.Value - MarginRight };
+            ValueScale.Output = new ScaleRange { Start = Height.Value - MarginBottom, End = MarginTop };
+
+            ValueScale.Fit(ValueAxis.TickDistance);
+            CategoryScale.Fit(CategoryAxis.TickDistance);
+
+            var stateHasChanged = !ValueScale.IsEqualTo(valueScale);
+
+            if (!CategoryScale.IsEqualTo(categoryScale))
+            {
+                stateHasChanged = true;
+            }
+
+            return stateHasChanged;
+        }
+    }
+}
