d60841b040923fc6f48ea1b87526eea6b9f35cf8
Vasil Yordanov <vasil@yordanov.info>
2024-10-25 16:17:47 +0300

    Add CardGroup component. Resolves #1658 (#1753)
    Add RadzenCardGroup component

---
+++ Radzen.Blazor/RadzenCardGroup.razor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Radzen.Blazor
+{
+    /// <summary>
+    /// RadzenCardGroup component.
+    /// </summary>
+    public partial class RadzenCardGroup : RadzenComponentWithChildren
+    {
+        /// <summary>
+        /// Toggles the responsive mode of the component. If set to <c>true</c> (the default) the component will be 
+        /// expanded on larger displays and collapsed on touch devices. Set to <c>false</c> if you want to disable this behavior.
+        /// </summary>
+        [Parameter]
+        public bool Responsive { get; set; } = true;
+        
+        /// <inheritdoc />
+        protected override string GetComponentCssClass()
+        {
+            var classList = new List<string>();
+            classList.Add("rz-card-group");
+
+            if (Responsive)
+            {
+                classList.Add("rz-card-group-responsive");
+            }
+
+            return string.Join(" ", classList);
+        }
+    }
+}
