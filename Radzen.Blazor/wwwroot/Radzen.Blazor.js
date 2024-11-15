a3fbda188d4fc6d13d4a4621ea938057f2628fad
Ezekiel768 <67689152+Ezekiel768@users.noreply.github.com>
2024-11-15 11:43:05 +0200

    Update Radzen.Blazor.js
    Fix startup exception

--- Radzen.Blazor/wwwroot/Radzen.Blazor.js
+++ Radzen.Blazor/wwwroot/Radzen.Blazor.js
@@ -16,11 +16,13 @@ if (!Element.prototype.closest) {
   };
 }
 
-if (document.fonts) {
-  document.body.classList.add('rz-icons-loading');
-  document.fonts.load('16px Material Symbols').then(() => {
-      document.body.classList.remove('rz-icons-loading');
-  })
+document.onreadystatechange = function () {
+    if (document.fonts) {
+      document.body.classList.add('rz-icons-loading');
+      document.fonts.load('16px Material Symbols').then(() => {
+          document.body.classList.remove('rz-icons-loading');
+      })
+    }
 }
 
 var resolveCallbacks = [];
