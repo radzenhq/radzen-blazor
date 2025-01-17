9947cbf47bac27188e9cc2c642ca438fc648b659
Vladimir Enchev <vladimir.enchev@gmail.com>
2025-01-17 10:10:09 +0200

    RadzenDropDown with filtering on Android closes on open
    Fix #1913

--- Radzen.Blazor/wwwroot/Radzen.Blazor.js
+++ Radzen.Blazor/wwwroot/Radzen.Blazor.js
@@ -1228,7 +1228,7 @@ window.Radzen = {
         }
 
         var closestLink = e.target.closest && (e.target.closest('.rz-link') || e.target.closest('.rz-navigation-item-link'));
-        if (e.type == 'resize' || closestLink && closestLink.closest && closestLink.closest('a')) {
+        if (e.type == 'resize' && !/Android/i.test(navigator.userAgent) || closestLink && closestLink.closest && closestLink.closest('a')) {
             if (Radzen.closeAllPopups) {
                 Radzen.closeAllPopups();
             }
