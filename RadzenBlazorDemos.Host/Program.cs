65f20c232c1b6e28a02b27d882e5c64a4974fcbd
Atanas Korchev <akorchev@gmail.com>
2025-01-20 17:46:25 +0200

    Linking to an anchor does not scroll to the anchor.

--- RadzenBlazorDemos.Host/Program.cs
+++ RadzenBlazorDemos.Host/Program.cs
@@ -92,6 +92,7 @@ app.UseRequestLocalization(new RequestLocalizationOptions
 app.UseHttpsRedirection();
 app.UseDefaultFiles();
 app.MapStaticAssets();
+app.UseStaticFiles();
 app.UseRouting();
 app.UseAntiforgery();
 app.MapRazorPages();
