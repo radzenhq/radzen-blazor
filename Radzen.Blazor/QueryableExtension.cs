1912e693755b157f6b268596f5c26c94b19c3170
Vladimir Enchev <vladimir.enchev@gmail.com>
2025-02-14 10:28:35 +0200

    OrderBy() variable reference ignored if present

--- Radzen.Blazor/QueryableExtension.cs
+++ Radzen.Blazor/QueryableExtension.cs
@@ -7,6 +7,7 @@ using System.Globalization;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace Radzen
 {
@@ -95,12 +96,23 @@ namespace Radzen
             return (IQueryable<T>)OrderBy((IQueryable)source, ordering);
         }
 
+        static string RemoveVariableReference(string expression)
+        {
+            // Regex pattern to match any variable reference in a lambda expression
+            string pattern = @"(\b\w+\b)\s*=>\s*\1\.(\w+)(\s*\?\?\s*\1\.\w+)?";
+
+            // Replace with just the property name, ignoring the variable reference
+            return Regex.Replace(expression, pattern, m => m.Groups[2].Value);
+        }
+
         /// <summary>
         /// Sorts the elements of a sequence in ascending or descending order according to a key.
         /// </summary>
         /// <returns>A <see cref="IQueryable"/> whose elements are sorted according to the specified <paramref name="ordering"/>.</returns>
         public static IQueryable OrderBy(this IQueryable source, string ordering = null)
         {
+            ordering = RemoveVariableReference(ordering ?? "");
+
             var parameters = new ParameterExpression[] { Expression.Parameter(source.ElementType, "x") };
 
             Expression expression = source.Expression;
