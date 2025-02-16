34d4ec4d653db04aa0d9e62e140bf04174c91c87
Atanas Korchev <akorchev@gmail.com>
2025-02-16 15:07:39 +0200

    Add ExpressionParser and tests.

---
+++ Radzen.Blazor.Tests/ExpressionParserTests.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Radzen.Blazor.Tests
+{
+  public enum CarType
+    {
+      Sedan,
+      Coupe
+    }
+
+  public class ExpressionParserTests
+  {
+    class Person
+    {
+      public string Name { get; set; }
+    }
+
+    public class Car
+    {
+      public CarType Type { get; set; }
+    }
+
+
+    class OrderDetail
+    {
+      public Product Product { get; set; }
+      public int Quantity { get; set; }
+      public Order Order { get; set; }
+
+      public List<WorkStatus> WorkStatuses { get; set; }
+    }
+
+    class Order
+    {
+      public DateTime OrderDate { get; set; }
+    }
+
+    class Product
+    {
+      public string ProductName { get; set; }
+    }
+
+    [Fact]
+    public void Should_ParseBindaryExpression()
+    {
+      var expression = ExpressionParser.Parse<Person>("p => p.Name == \"foo\"");
+
+      var func = expression.Compile();
+
+      Assert.True(func(new Person() { Name = "foo" }));
+    }
+
+    [Fact]
+    public void Should_ParseConditionalExpression()
+    {
+      var expression = ExpressionParser.Parse<OrderDetail>("it => (it.Product.ProductName == null ? \"\" : it.Product.ProductName).Contains(\"Queso\") && it.Quantity == 50");
+
+      var func = expression.Compile();
+
+      Assert.True(func(new OrderDetail() { Product = new Product { ProductName = "Queso" }, Quantity = 50 }));
+    }
+
+    [Fact]
+    public void Should_ParseNestedLogicalOperations()
+    {
+      var expression = ExpressionParser.Parse<OrderDetail>("it => (it.Product.ProductName == null ? \"\" : it.Product.ProductName).Contains(\"Queso\") && (it.Quantity == 50 || it.Quantity == 12)");
+
+      var func = expression.Compile();
+
+      Assert.True(func(new OrderDetail() { Product = new Product { ProductName = "Queso" }, Quantity = 12 }));
+
+    }
+
+    [Fact]
+    public void Should_SupportDateTimeParsing()
+    {
+      var expression = ExpressionParser.Parse<OrderDetail>("it => it.Order.OrderDate >= DateTime.Parse(\"2025-02-11\")");
+
+      var func = expression.Compile();
+      Assert.True(func(new OrderDetail { Order = new Order { OrderDate = new DateTime(2025, 2, 11) } }));
+    }
+
+    class ItemWithGenericProperty<T>
+    {
+      public T Value { get; set; }
+    }
+
+    [Fact]
+    public void Should_SupportDateOnlyParsing()
+    {
+      var expression = ExpressionParser.Parse<ItemWithGenericProperty<DateOnly>>("it => it.Value >= DateOnly.Parse(\"2025-02-11\")");
+
+      var func = expression.Compile();
+
+      Assert.True(func(new ItemWithGenericProperty<DateOnly> { Value = new DateOnly(2025, 2, 11) }));
+    }
+
+    [Fact]
+    public void Should_SupportTimeOnlyParsing()
+    {
+      var expression = ExpressionParser.Parse<ItemWithGenericProperty<TimeOnly>>("it => it.Value >= TimeOnly.Parse(\"12:00:00\")");
+      var func = expression.Compile();
+      Assert.True(func(new ItemWithGenericProperty<TimeOnly> { Value = new TimeOnly(12, 0, 0) }));
+    }
+
+    [Fact]
+    public void Should_SupportGuidParsing()
+    {
+      var expression = ExpressionParser.Parse<ItemWithGenericProperty<Guid>>("it => it.Value == Guid.Parse(\"f0e7e7d8-4f4d-4b5f-8b3e-3f1d1b4f5f5f\")");
+      var func = expression.Compile();
+      Assert.True(func(new ItemWithGenericProperty<Guid> { Value = Guid.Parse("f0e7e7d8-4f4d-4b5f-8b3e-3f1d1b4f5f5f") }));
+    }
+
+    [Fact]
+    public void Should_SupportEnumWithCasts()
+    {
+      var expression = ExpressionParser.Parse<Car>($"it => it.Type == (Radzen.Blazor.Tests.CarType)1", type =>
+      {
+        return Type.GetType(type);
+      });
+
+      var func = expression.Compile();
+
+      Assert.True(func(new Car { Type = CarType.Coupe }));
+    }
+
+    [Fact]
+    public void Should_SupportCollectionLiterals()
+    {
+      var expression = ExpressionParser.Parse<OrderDetail>("it => (new []{\"Tofu\"}).Contains(it.Product.ProductName)");
+      var func = expression.Compile();
+
+      Assert.True(func(new OrderDetail { Product = new Product { ProductName = "Tofu" } }));
+    }
+
+    class WorkStatus
+    {
+      public string Name { get; set; }
+    }
+
+
+    [Fact]
+    public void Should_SupportNestedLambdas()
+    {
+      var expression = ExpressionParser.Parse<OrderDetail>("it => it.WorkStatuses.Any(i => (new []{\"Office\"}).Contains(i.Name))");
+      var func = expression.Compile();
+
+      Assert.True(func(new OrderDetail { WorkStatuses = [new() { Name = "Office" }] }));
+    }
+
+    [Fact]
+    public void Should_SupportToLower()
+    {
+      var expression = ExpressionParser.Parse<Person>("it => (it.Name == null ? \"\" : it.Name).ToLower().Contains(\"na\".ToLower())");
+
+      var func = expression.Compile();
+
+      Assert.True(func(new Person { Name = "Nana" }));
+    }
+  }
+}
---
+++ Radzen.Blazor/ExpressionParser.cs
@@ -0,0 +1,226 @@
+using System;
+using System.Linq.Expressions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+
+namespace Radzen;
+
+class ExpressionSyntaxVisitor : CSharpSyntaxVisitor<Expression>
+{
+  private readonly ParameterExpression parameter;
+  private readonly Func<string, Type> typeLocator;
+
+  public ExpressionSyntaxVisitor(ParameterExpression parameter, Func<string, Type> typeLocator)
+  {
+    this.parameter = parameter;
+    this.typeLocator = typeLocator;
+  }
+
+  public override Expression VisitBinaryExpression(BinaryExpressionSyntax node)
+  {
+    var left = Visit(node.Left);
+    var right = Visit(node.Right);
+    return Expression.MakeBinary(ParseBinaryOperator(node.OperatorToken), left, right);
+  }
+
+  public override Expression VisitMemberAccessExpression(MemberAccessExpressionSyntax node)
+  {
+    var expression = Visit(node.Expression) ?? parameter;
+    return Expression.PropertyOrField(expression, node.Name.Identifier.Text);
+  }
+
+  public override Expression VisitLiteralExpression(LiteralExpressionSyntax node)
+  {
+    return Expression.Constant(ParseLiteral(node));
+  }
+
+  public override Expression VisitIdentifierName(IdentifierNameSyntax node)
+  {
+    if (node.Identifier.Text == parameter.Name)
+    {
+      return parameter;
+    }
+
+    return node.Identifier.Text switch
+    {
+      nameof(DateTime) => Expression.Constant(typeof(DateTime)),
+      nameof(DateOnly) => Expression.Constant(typeof(DateOnly)),
+      nameof(TimeOnly) => Expression.Constant(typeof(TimeOnly)),
+      nameof(Guid) => Expression.Constant(typeof(Guid)),
+      _ => throw new NotSupportedException("Unsupported identifier: " + node.Identifier.Text),
+    };
+  }
+
+  public override Expression VisitConditionalExpression(ConditionalExpressionSyntax node)
+  {
+    var condition = Visit(node.Condition);
+    var whenTrue = Visit(node.WhenTrue);
+    var whenFalse = Visit(node.WhenFalse);
+    return Expression.Condition(condition, whenTrue, whenFalse);
+  }
+
+  public override Expression VisitParenthesizedExpression(ParenthesizedExpressionSyntax node)
+  {
+    return Visit(node.Expression);
+  }
+
+  public override Expression VisitCastExpression(CastExpressionSyntax node)
+  {
+    var targetType = typeLocator?.Invoke(node.Type.ToString());
+
+    if (targetType == null)
+    {
+      throw new NotSupportedException("Unsupported cast type: " + node.Type);
+    }
+
+    var operand = Visit(node.Expression);
+    return Expression.Convert(operand, targetType);
+  }
+
+  public override Expression VisitImplicitArrayCreationExpression(ImplicitArrayCreationExpressionSyntax node)
+  {
+    var expressions = node.Initializer.Expressions.Select(Visit).ToArray();
+    var elementType = expressions.Length > 0 ? expressions[0].Type : typeof(object);
+    return Expression.NewArrayInit(elementType, expressions);
+  }
+
+  private Expression CallStaticMethod(Type type, string methodName, Expression[] arguments, Type[] argumentTypes)
+  {
+    var methodInfo = type.GetMethod(methodName, BindingFlags.Public | BindingFlags.Static, argumentTypes);
+
+    if (methodInfo != null)
+    {
+      return Expression.Call(methodInfo, arguments);
+    }
+
+    throw new NotSupportedException("Method not found: " + methodName);
+  }
+
+  public override Expression DefaultVisit(SyntaxNode node)
+  {
+    throw new NotSupportedException("Unsupported syntax: " + node.GetType().Name);
+  }
+
+  public override Expression VisitSimpleLambdaExpression(SimpleLambdaExpressionSyntax node)
+  {
+    var body = Visit(node.Body);
+
+    return Expression.Lambda(body, parameter);
+  }
+
+  private Expression VisitArgument(Expression instance, ArgumentSyntax argument)
+  {
+    if (argument.Expression is SimpleLambdaExpressionSyntax lambda)
+    {
+      var itemType = GetItemType(instance.Type);
+
+      var visitor = new ExpressionSyntaxVisitor(Expression.Parameter(itemType, lambda.Parameter.Identifier.Text), typeLocator);
+
+      return visitor.Visit(lambda);
+    }
+
+    return Visit(argument.Expression);
+  }
+
+  public override Expression VisitInvocationExpression(InvocationExpressionSyntax node)
+  {
+    if (node.Expression is MemberAccessExpressionSyntax methodCall)
+    {
+      var instance = Visit(methodCall.Expression);
+      var arguments = node.ArgumentList.Arguments.Select(a => VisitArgument(instance, a)).ToArray();
+      var argumentTypes = arguments.Select(a => a.Type).ToArray();
+
+      if (instance is ConstantExpression constant && constant.Value is Type type)
+      {
+        return CallStaticMethod(type, methodCall.Name.Identifier.Text, arguments, argumentTypes);
+      }
+
+      var instanceType = instance.Type;
+      var methodInfo = instanceType.GetMethod(methodCall.Name.Identifier.Text, BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static, argumentTypes);
+
+      if (methodInfo == null)
+      {
+        methodInfo = typeof(Enumerable)
+            .GetMethods(BindingFlags.Public | BindingFlags.Static)
+            .FirstOrDefault(m => m.Name == methodCall.Name.Identifier.Text && m.GetParameters().Length == arguments.Length + 1);
+
+        if (methodInfo != null)
+        {
+          var itemType = GetItemType(instanceType);
+          var genericMethod = methodInfo.MakeGenericMethod(itemType);
+          return Expression.Call(genericMethod, new[] { instance }.Concat(arguments));
+        }
+      }
+
+      if (methodInfo == null)
+      {
+        throw new NotSupportedException("Unsupported method call: " + methodCall.Name.Identifier.Text);
+      }
+
+      return Expression.Call(instance, methodInfo, arguments);
+    }
+
+    throw new NotSupportedException("Unsupported invocation expression: " + node.ToString());
+  }
+
+  private static Type GetItemType(Type enumerableOrArray)
+  {
+    return enumerableOrArray.IsArray ? enumerableOrArray.GetElementType() : enumerableOrArray.GetGenericArguments()[0];
+  }
+
+
+  private static object ParseLiteral(LiteralExpressionSyntax literal)
+  {
+    return literal.Kind() switch
+    {
+      SyntaxKind.StringLiteralExpression => literal.Token.ValueText,
+      SyntaxKind.NumericLiteralExpression => literal.Token.Value,
+      SyntaxKind.TrueLiteralExpression => true,
+      SyntaxKind.FalseLiteralExpression => false,
+      SyntaxKind.NullLiteralExpression => null,
+      _ => throw new NotSupportedException("Unsupported literal: " + literal),
+    };
+  }
+
+  private static ExpressionType ParseBinaryOperator(SyntaxToken token)
+  {
+    return token.Kind() switch
+    {
+      SyntaxKind.EqualsEqualsToken => ExpressionType.Equal,
+      SyntaxKind.LessThanToken => ExpressionType.LessThan,
+      SyntaxKind.GreaterThanToken => ExpressionType.GreaterThan,
+      SyntaxKind.LessThanEqualsToken => ExpressionType.LessThanOrEqual,
+      SyntaxKind.GreaterThanEqualsToken => ExpressionType.GreaterThanOrEqual,
+      SyntaxKind.ExclamationEqualsToken => ExpressionType.NotEqual,
+      SyntaxKind.AmpersandAmpersandToken => ExpressionType.AndAlso,
+      SyntaxKind.BarBarToken => ExpressionType.OrElse,
+      _ => throw new NotSupportedException("Unsupported operator: " + token.Text),
+    };
+  }
+}
+
+public static class ExpressionParser
+{
+  public static Expression<Func<T, bool>> Parse<T>(string expression, Func<string, Type> typeLocator = null)
+  {
+    var syntaxTree = CSharpSyntaxTree.ParseText(expression);
+    var root = syntaxTree.GetRoot();
+
+    var lambdaExpression = root.DescendantNodes().OfType<SimpleLambdaExpressionSyntax>().FirstOrDefault();
+
+    if (lambdaExpression == null)
+    {
+      throw new ArgumentException("Invalid lambda expression.");
+    }
+
+    var parameter = Expression.Parameter(typeof(T), lambdaExpression.Parameter.Identifier.Text);
+
+    var visitor = new ExpressionSyntaxVisitor(parameter, typeLocator);
+    var body = visitor.Visit(lambdaExpression.Body);
+
+    return Expression.Lambda<Func<T, bool>>(body, parameter);
+  }
+}
