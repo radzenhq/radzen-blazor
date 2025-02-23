dac09a3d7521f5446005dc5ac1c2b6f5cc1f94de
Pedro Constantino <pedro.constantino@enerwatt.com.br>
2025-02-23 16:07:34 -0300

    [IMPROVEMENT] - Adding HeaderWhiteSpaceText and WhiteSpaceText properties to RadzenDataGridColumn.

---
+++ Radzen.Blazor/WhiteSpaceText.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radzen.Blazor;
+
+/// <summary>
+/// Specifies the white space text used when rendering the text.
+/// </summary>
+public enum WhiteSpaceText
+{
+    /// <summary>
+    /// The text will wrap when necessary.
+    /// </summary>
+    Wrap,
+    /// <summary>
+    /// The text will not wrap.
+    /// </summary>
+    Nowrap,
+    /// <summary>
+    /// The text will wrap when necessary, and any overflowed text will be clipped.
+    /// </summary>
+    Truncate
+}
