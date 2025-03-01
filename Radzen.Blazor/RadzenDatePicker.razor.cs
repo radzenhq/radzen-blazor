84954f1919078548590affd510e0e5ea8ee5240c
五味　凌一 <152822777+n4213-ndensan@users.noreply.github.com>
2025-03-01 17:44:05 +0900

    Added “YearFormatter” property in RadzenDatePicker to allow custom formatting of Year (#2008)
    * Added “YearFormatter” property to allow custom formatting of Year
    
    This enhancement aims to enable immediate response to changes in the era name without relying on .NET’s built-in implementation.
    
    * Update YearFormatter property initialization and usage
    
    Changed the initial value of the YearFormatter property from null to the FormatYear method. Added a constructor to RadzenDatePicker where the YearFormatter property is set to the FormatYear method.
    
    * Add xml comment.

--- Radzen.Blazor/RadzenDatePicker.razor.cs
+++ Radzen.Blazor/RadzenDatePicker.razor.cs
@@ -285,7 +285,7 @@ namespace Radzen.Blazor
             YearTo = max.HasValue ? max.Value.Year : int.Parse(YearRange.Split(':').Last());
             months = Enumerable.Range(1, 12).Select(i => new NameValue() { Name = Culture.DateTimeFormat.GetMonthName(i), Value = i }).ToList();
             years = Enumerable.Range(YearFrom, YearTo - YearFrom + 1)
-                .Select(i => new NameValue() { Name = FormatYear(i), Value = i }).ToList();
+                .Select(i => new NameValue() { Name = YearFormatter(i), Value = i }).ToList();
         }
 
         private string FormatYear(int year)
@@ -296,6 +296,21 @@ namespace Radzen.Blazor
 
             return date.ToString(YearFormat, Culture);
         }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RadzenDatePicker{TValue}"/> class.
+        /// </summary>
+        public RadzenDatePicker()
+        {
+            YearFormatter = FormatYear;
+        }
+
+        /// <summary>
+        /// Gets or sets the year formatter. Set to <c>FormatYear</c> by default.
+        /// If set, this function will take precedence over <see cref="YearFormat"/>.
+        /// </summary>
+        [Parameter]
+        public Func<int, string> YearFormatter { get; set; }
 
         /// <summary>
         /// Gets ot sets the year format. Set to <c>yyyy</c> by default.
