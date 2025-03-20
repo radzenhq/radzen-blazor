45d95d7b457b413dffd3d90c742a206bd657ecff
Nopke <44393502+Cosmatevs@users.noreply.github.com>
2025-03-20 09:37:43 +0100

    New component: TimeSpanPicker (#2037)
    * TimeSpanPicker: initial
    
    * TimeSpanPicker: adjust material themes, handle long labels
    
    * TimeSpanPicker: fix value set, move invoking Change events for consistency with other components, update demos
    
    * TimeSpanPicker: fix preventKeyPress, adjust styles
    
    * TimeSpanPicker: adjust demos and add new
    
    * PopupOrInline: handle Inline parameter change
    
    * TimeSpanPicker: change icon and improve handling Disabled/ReadMe parameters in panel
    
    * TimeSpanPicker: update demos
    
    * TimeSpanPicker: add to the example panel
    
    * DatePicker: add a tag and info about time picker
    
    * TimeSpanPicker: minor code cleanup, adjust styles, open popup on click if input not allowed
    
    * * TimeSpanPicker: minor style and code adjustments
    * DatePicker: add readonly class only if not disabled
    
    * TimeSpanPicker: add to the index page
    
    * TimeSpanPicker: adjust style code, use proper minus sign
    
    * TimeSpanPicker:
    * read input value without using JS
    * fix Value and ConfirmedValue inconsistency
    
    * TimeSpanPicker: always refresh the displayed value on input change
    
    * TimeSpanPicker: add some tests (not finished yet)
    
    * Numeric: fix culture-dependent test fail
    
    * TimeSpanPicker: reorganize parameters
    
    * TimeSpanPicker tests: cover input parameters, add regions
    
    * TimeSpanPicker: tweak regions of input and panel parameters
    
    * TimeSpanPicker tests: reorganize regions, use more specific selectors
    
    * TimeSpanPicker: improve value change and popup toggle prevention
    
    * TimeSpanPicker:
    * use culture while parsing
    * use consistent culture in tests
    
    * FormField: add TimeSpanPicker in the demo
    
    * TimeSpanPicker: more tests
    
    * revert unrelated and unintended changed
    
    * TimeSpanPicker test: move theory data to properties, add step tests
    
    * TimeSpanPicker test: minor code cleanup, add remaining label tests
    
    * TimeSpanPicker test: use dictionary of unit element selectors
    
    * TimeSpanPicker test: create separate methods instead passing field expressions as theory, add more regions
    
    * TimeSpanPicker: rename properties to match the convention and update their summaries
    
    * TimeSpanPicker: remove UnconfirmedValueChanged param
    TimeSpanPicker test: add event-related tests for the panel, simplify some tests related to timespan units
    
    * TimeSpanPicker: pass culture to numeric fields, add panel field rendering tests
    
    * TimeSpanPicker: unify unit fields under one RenderFragment function
    
    * TimeSpanPicker: fix missing microseconds picker, fix classes of input fields
    
    * TimeSpanPicker:
    * prevent from changing UnconfirmedValue externally by changing Value,
    * prevent from raising ValueChanged and Change events if the value haven't actually change
    
    * revert unintended changes
    
    * TimeSpanPicker, DatePicker: fix icon positions in FormFields having filled and flat variants
    
    * TimeSpanPicker: make AllowClear false by default, like in DatePicker
    
    * revert unintended change in demos project
    
    * * remove PopupOrInline component
    * revert changes in Popup component
    * TimeSpanPicker: reimplement inline/popup handling

--- RadzenBlazorDemos/Services/ExampleService.cs
+++ RadzenBlazorDemos/Services/ExampleService.cs
@@ -1412,9 +1412,9 @@ namespace RadzenBlazorDemos
                     Name = "DatePicker",
                     Path = "datepicker",
                     Updated = true,
-                    Description = "Demonstration and configuration of the Radzen Blazor Datepicker component with calendar mode.",
+                    Description = "Demonstration and configuration of the Radzen Blazor Datepicker component with calendar mode. Time Picker.",
                     Icon = "&#xe916",
-                    Tags = new [] { "calendar", "form", "edit" }
+                    Tags = new [] { "calendar", "time", "form", "edit" }
                 },
                 new Example
                 {
@@ -1646,6 +1646,15 @@ namespace RadzenBlazorDemos
                     Description = "Demonstration and configuration of the Radzen Blazor TextBox input component.",
                     Icon = "&#xe9f1",
                     Tags = new [] { "input", "form", "edit" }
+                },
+                new Example
+                {
+                    Name = "TimeSpanPicker",
+                    Path = "timespanpicker",
+                    New = true,
+                    Description = "Demonstration and configuration of the Radzen Blazor TimeSpanPicker component.",
+                    Icon = "&#xe425",
+                    Tags = new [] { "duration", "form", "edit" }
                 },
                 new Example
                 {
