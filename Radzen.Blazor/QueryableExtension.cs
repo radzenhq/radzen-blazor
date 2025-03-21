958fb43ac20f3291040e8c2cd60a129fef537d0b
Vladimir Enchev <vladimir.enchev@gmail.com>
2025-03-21 07:04:17 +0200

    Should not use Coalesce in OrderBy()

--- Radzen.Blazor/QueryableExtension.cs
+++ Radzen.Blazor/QueryableExtension.cs
@@ -168,7 +168,7 @@ namespace Radzen
                 expression = Expression.Call(
                     typeof(Queryable), order.Equals(sortStrings.First(), StringComparison.OrdinalIgnoreCase) ? methodAsc : methodDesc,
                     new Type[] { source.ElementType, property.Type },
-                    expression, Expression.Quote(Expression.Lambda(nameAndOrder.Contains(".") ? notNullCheck(property) : property, parameters)));
+                    expression, Expression.Quote(Expression.Lambda(property, parameters)));
 
                 methodAsc = "ThenBy";
                 methodDesc = "ThenByDescending";
