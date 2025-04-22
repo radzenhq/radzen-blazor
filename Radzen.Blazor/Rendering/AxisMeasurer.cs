bc7b0a9bdb36a40eee92a1a4741e307ac86dd601
Atanas Korchev <akorchev@gmail.com>
2025-04-22 10:36:02 +0300

    Take axis label rotation into account when measuring the category axis size.

--- Radzen.Blazor/Rendering/AxisMeasurer.cs
+++ Radzen.Blazor/Rendering/AxisMeasurer.cs
@@ -3,12 +3,12 @@ using System;
 namespace Radzen.Blazor.Rendering
 {
     /// <summary>
-    /// Class AxisMeasurer.
+    /// Measures the sises of the chart axes.
     /// </summary>
     public static class AxisMeasurer
     {
         /// <summary>
-        /// ies the axis.
+        /// Calculates the length of the Y axis.
         /// </summary>
         /// <param name="scale">The scale.</param>
         /// <param name="axis">The axis.</param>
@@ -38,13 +38,35 @@ namespace Radzen.Blazor.Rendering
         }
 
         /// <summary>
-        /// xes the axis.
+        /// Calculates the length of the X axis.
         /// </summary>
+        /// <param name="scale">The scale.</param>
+        /// <param name="axis">The axis.</param>
         /// <param name="title">The title.</param>
         /// <returns>System.Double.</returns>
-        public static double XAxis(RadzenAxisTitle title)
+        public static double XAxis(ScaleBase scale, AxisBase axis, RadzenAxisTitle title)
         {
             var size = 16 * 0.875 + 12;
+
+            var angle = axis.LabelRotation ?? axis.LabelAutoRotation;
+
+            if (angle != null)
+            {
+                var ticks = scale.Ticks(axis.TickDistance);
+
+                double length = 0;
+
+                for (var y = ticks.Start; y <= ticks.End; y += ticks.Step)
+                {
+                    var text = axis.Format(scale, y);
+
+                    length = Math.Max(length, TextMeasurer.TextWidth(text));
+                }
+
+                var alpha = Math.Abs(angle.Value) * Math.PI / 180;
+                var rotatedWidth = Math.Abs(Math.Sin(alpha) * length) + Math.Abs(Math.Cos(alpha) * size);
+                size = Math.Max(size, rotatedWidth);
+            }
 
             if (!String.IsNullOrEmpty(title.Text))
             {
