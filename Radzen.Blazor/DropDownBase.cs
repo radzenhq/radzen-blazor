65a78125b29ceb935245693df4d1716bfa21e1f3
Vladimir Enchev <vladimir.enchev@gmail.com>
2025-04-30 11:45:09 +0300

    DropDownBase select item as you type fixed

--- Radzen.Blazor/DropDownBase.cs
+++ Radzen.Blazor/DropDownBase.cs
@@ -745,7 +745,7 @@ namespace Radzen
 
                 Debounce(DebounceFilter, FilterDelay);
             }
-            else if(isFilter)
+            else if(!args.CtrlKey && !args.AltKey)
             {
                 var filteredItems = (!string.IsNullOrEmpty(TextProperty) ?
                     Query.Where(TextProperty, args.Key, StringFilterOperator.StartsWith, FilterCaseSensitivity.CaseInsensitive) :
