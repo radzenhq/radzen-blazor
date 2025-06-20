429fced7fa2d22e0499d0a0a254606cc657dc6cb
Atanas Korchev <akorchev@gmail.com>
2025-06-20 12:24:30 +0300

    Move spreadsheet implementation.

---
+++ Radzen.Blazor/Spreadsheet/Format.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Text;
+
+namespace Radzen.Blazor.Spreadsheet;
+
+#nullable enable
+/// <summary>
+/// Represents a format that can be applied to cells in a spreadsheet.
+/// </summary>
+public class Format
+{
+    private string? color;
+
+    /// <summary>
+    /// Gets or sets the color of the text in the format.
+    /// </summary>
+    public string? Color
+    {
+        get => color;
+        set
+        {
+            if (color != value)
+            {
+                color = value;
+                Changed?.Invoke();
+            }
+        }
+    }
+
+    private string? background;
+
+    /// <summary>
+    /// Gets or sets the background color of the format.
+    /// </summary>
+    public string? BackgroundColor
+    {
+        get => background;
+        set
+        {
+            if (background != value)
+            {
+                background = value;
+                Changed?.Invoke();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Occurs when the format is changed, allowing for updates to be made to the UI or other components that depend on this format.
+    /// </summary>
+    public event Action? Changed;
+
+    /// <summary>
+    /// Appends the CSS styles defined by this format to the provided StringBuilder.
+    /// </summary>
+    /// <param name="sb"></param>
+    public void AppendStyle(StringBuilder sb)
+    {
+        if (Color != null)
+        {
+            sb.Append("color: ");
+            sb.Append(Color);
+            sb.Append(';');
+        }
+
+        if (BackgroundColor != null)
+        {
+            if (sb.Length > 0)
+            {
+                sb.Append(' ');
+            }
+            sb.Append("background-color: ");
+            sb.Append(BackgroundColor);
+            sb.Append(';');
+        }
+    }
+}
