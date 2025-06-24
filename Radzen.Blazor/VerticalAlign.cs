ca0806d5b16c4c28d8fea99be8e62821dbcf7d26
Atanas Korchev <akorchev@gmail.com>
2025-06-24 13:26:27 +0300

    Vertical align tool.

---
+++ Radzen.Blazor/VerticalAlign.cs
@@ -0,0 +1,20 @@
+namespace Radzen;
+
+/// <summary>
+/// Specifies vertical alignment of content in a container.
+/// </summary>
+public enum VerticalAlign
+{
+    /// <summary>
+    /// The content is aligned to the top of its container.
+    /// </summary>
+    Top,
+    /// <summary>
+    /// The content is vertically centered in its container.
+    /// </summary>
+    Middle,
+    /// <summary>
+    /// The content is aligned to the bottom of its container.
+    /// </summary>
+    Bottom
+}
