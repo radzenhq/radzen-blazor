a87d7464783201610d9f9e64c912a0cc2c8c2162
Atanas Korchev <akorchev@gmail.com>
2025-06-27 20:10:14 +0300

    Add show all and blank options.

--- Radzen.Blazor/Spreadsheet/CellMenu.razor.cs
+++ Radzen.Blazor/Spreadsheet/CellMenu.razor.cs
@@ -63,10 +63,28 @@ public partial class CellMenu : ComponentBase
             filter.Criterion.Accept(visitor);
         }
 
-        // Add any found values to the selected set
-        foreach (var value in visitor.FoundValues)
+        // If there are existing filter values for this column, use them
+        if (visitor.FoundValues.Any())
         {
-            selectedFilterValues.Add(value);
+            foreach (var value in visitor.FoundValues)
+            {
+                selectedFilterValues.Add(value);
+            }
+        }
+        else
+        {
+            // No existing filters for this column, so select all available values by default
+            var availableValues = LoadAvailableValues();
+            foreach (var value in availableValues)
+            {
+                selectedFilterValues.Add(value.Value);
+            }
+            
+            // Also select null if blank option should be shown
+            if (ShouldShowBlankOption())
+            {
+                selectedFilterValues.Add(null);
+            }
         }
     }
 
@@ -157,11 +175,118 @@ public partial class CellMenu : ComponentBase
         {
             selectedFilterValues.Remove(value);
         }
+        
+        StateHasChanged();
     }
 
     private bool IsValueSelected(object value)
     {
         return selectedFilterValues.Contains(value);
+    }
+
+    private bool? IsSelectAllChecked()
+    {
+        var availableValues = LoadAvailableValues();
+        var showBlank = ShouldShowBlankOption();
+        
+        if (!availableValues.Any() && !showBlank)
+            return false;
+
+        var totalItems = availableValues.Count + (showBlank ? 1 : 0);
+        var selectedCount = availableValues.Count(v => selectedFilterValues.Contains(v.Value));
+        
+        // Add 1 to selected count if blank is selected
+        if (showBlank && selectedFilterValues.Contains(null))
+        {
+            selectedCount++;
+        }
+        
+        if (selectedCount == 0)
+            return false;
+        else if (selectedCount == totalItems)
+            return true;
+        else
+            return null; // Indeterminate state
+    }
+
+    private void OnSelectAllChanged(bool? isChecked)
+    {
+        var availableValues = LoadAvailableValues();
+        var showBlank = ShouldShowBlankOption();
+        
+        // For tristate checkbox behavior:
+        // - When indeterminate (null) and clicked -> becomes checked (true) -> select all
+        // - When checked (true) and clicked -> becomes unchecked (false) -> deselect all
+        // - When unchecked (false) and clicked -> becomes checked (true) -> select all
+        if (isChecked != false)
+        {
+            // Select all values
+            foreach (var value in availableValues)
+            {
+                selectedFilterValues.Add(value.Value);
+            }
+            
+            // Also select blank if it should be shown
+            if (showBlank)
+            {
+                selectedFilterValues.Add(null);
+            }
+        }
+        else if (isChecked == false)
+        {
+            // Deselect all values
+            foreach (var value in availableValues)
+            {
+                selectedFilterValues.Remove(value.Value);
+            }
+            
+            // Also deselect blank
+            selectedFilterValues.Remove(null);
+        }
+        // Note: isChecked == null (indeterminate) should not happen in the Change event
+        // as clicking indeterminate should transition to true
+        
+        StateHasChanged();
+    }
+
+    private bool ShouldShowBlankOption()
+    {
+        var dataTable = GetCurrentDataTable();
+        if (dataTable == null) return false;
+
+        // Check if any cell in the column has null or empty value
+        for (int row = dataTable.Range.Start.Row; row <= dataTable.Range.End.Row; row++)
+        {
+            var cell = Sheet.Cells[row, Column];
+            var value = cell.Value;
+            var text = cell.GetValue();
+            
+            if (value == null || string.IsNullOrEmpty(text))
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+    private bool IsBlankSelected()
+    {
+        return selectedFilterValues.Contains(null);
+    }
+
+    private void OnBlankSelectionChanged(bool isChecked)
+    {
+        if (isChecked)
+        {
+            selectedFilterValues.Add(null);
+        }
+        else
+        {
+            selectedFilterValues.Remove(null);
+        }
+        
+        StateHasChanged();
     }
 
     private async Task OnApplyFilterAsync()
@@ -178,12 +303,12 @@ public partial class CellMenu : ComponentBase
                 );
 
                 var filter = new SheetFilter(
-                new InListCriterion
-                {
-                    Column = Column,
-                    Values = [.. selectedFilterValues]
-                },
-                columnRange
+                    new InListCriterion
+                    {
+                        Column = Column,
+                        Values = [.. selectedFilterValues]
+                    },
+                    columnRange
                 );
 
                 Sheet.AddFilter(filter);
