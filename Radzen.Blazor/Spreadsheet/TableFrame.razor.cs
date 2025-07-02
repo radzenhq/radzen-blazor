3a2a21dcf652a59abb84fcdbcd5134c7e5a3908c
Atanas Korchev <akorchev@gmail.com>
2025-07-02 09:58:27 +0300

    Rename DataTable to Table.

--- Radzen.Blazor/Spreadsheet/TableFrame.razor.cs
+++ Radzen.Blazor/Spreadsheet/TableFrame.razor.cs
@@ -3,24 +3,24 @@ using Microsoft.AspNetCore.Components;
 namespace Radzen.Blazor.Spreadsheet;
 
 /// <summary>
-/// Renders a frame for a data table in a spreadsheet.
+/// Renders a frame for a table in a spreadsheet.
 /// </summary>
-public partial class DataTableFrame
+public partial class TableFrame
 {
     /// <summary>
-    /// Gets or sets the data table to be rendered in the frame.
+    /// Gets or sets the table to be rendered in the frame.
     /// </summary>
     [Parameter]
-    public DataTable DataTable { get; set; } = default!;
+    public Table Table { get; set; } = default!;
 
     /// <summary>
-    /// Gets or sets the sheet that contains the data table.
+    /// Gets or sets the sheet that contains the table.
     /// </summary>
     [Parameter]
     public Sheet Sheet { get; set; } = default!;
 
     /// <summary>
-    /// Gets or sets the context for the virtual grid that contains the data table.
+    /// Gets or sets the context for the virtual grid that contains the table.
     /// </summary>
     [Parameter]
     public IVirtualGridContext Context { get; set; } = default!;
