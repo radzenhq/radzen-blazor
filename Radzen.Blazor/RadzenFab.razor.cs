ba2aa9040089723a67291726a4b9af3863ce8426
Atanas Korchev <akorchev@gmail.com>
2025-09-11 17:15:20 +0300

    Reuse the rendering from RadzenButton.

--- Radzen.Blazor/RadzenFab.razor.cs
+++ Radzen.Blazor/RadzenFab.razor.cs
@@ -1,40 +1,31 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 using Radzen.Blazor.Rendering;
-using System.Threading.Tasks;
 
-namespace Radzen.Blazor
+namespace Radzen.Blazor;
+
+/// <summary>
+/// RadzenFab component.
+/// </summary>
+/// <example>
+/// <code>
+/// &lt;RadzenFab Icon="add" Click=@(args => Console.WriteLine("FAB clicked")) /&gt;
+/// &lt;RadzenFab Icon="add" IsBusy="@isLoading" BusyText="Loading..." Click=@OnFabClick /&gt;
+/// </code>
+/// </example>
+public partial class RadzenFab : RadzenButton
 {
-    /// <summary>
-    /// RadzenFab component.
-    /// </summary>
-    /// <example>
-    /// <code>
-    /// &lt;RadzenFab Icon="add" Click=@(args => Console.WriteLine("FAB clicked")) /&gt;
-    /// &lt;RadzenFab Icon="add" IsBusy="@isLoading" BusyText="Loading..." Click=@OnFabClick /&gt;
-    /// </code>
-    /// </example>
-    public partial class RadzenFab : RadzenButton
-    {
-        /// <summary>
-        /// Gets or sets the size. Defaults to Large for FAB components.
-        /// </summary>
-        /// <value>The size.</value>
-        public new ButtonSize Size { get; set; } = ButtonSize.Large;
+    /// <inheritdoc />
+    public override ButtonSize Size { get; set; } = ButtonSize.Large;
 
-        /// <summary>
-        /// Gets the component CSS class.
-        /// </summary>
-        protected override string GetComponentCssClass()
-        {
-            return ClassList.Create("rz-button rz-fab")
-                           .AddButtonSize(Size)
-                           .AddVariant(Variant)
-                           .AddButtonStyle(ButtonStyle)
-                           .AddDisabled(IsDisabled)
-                           .AddShade(Shade)
-                           .Add($"rz-button-icon-only", string.IsNullOrEmpty(Text) && !string.IsNullOrEmpty(Icon))
-                           .ToString();
-        }
+    /// <inheritdoc />
+    protected override string GetComponentCssClass()
+    {
+        return ClassList.Create("rz-button rz-fab")
+                       .AddButtonSize(Size)
+                       .AddVariant(Variant)
+                       .AddButtonStyle(ButtonStyle)
+                       .AddDisabled(IsDisabled)
+                       .AddShade(Shade)
+                       .Add($"rz-button-icon-only", string.IsNullOrEmpty(Text) && !string.IsNullOrEmpty(Icon))
+                       .ToString();
     }
-} 
+}
