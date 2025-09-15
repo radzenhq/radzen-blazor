a0f406bc03acab58c61c56a8c4702f801ebcb977
Atanas Korchev <akorchev@gmail.com>
2025-09-15 17:45:12 +0300

    Remove expressions from formulas.

--- Radzen.Blazor/Spreadsheet/Functions/NotFunction.cs
+++ Radzen.Blazor/Spreadsheet/Functions/NotFunction.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq.Expressions;
 
 #nullable enable
 
@@ -8,26 +7,22 @@ namespace Radzen.Blazor.Spreadsheet;
 
 class NotFunction : FormulaFunction
 {
-    public override Expression Evaluate(List<Expression> arguments)
+    public override object? Evaluate(List<object?> arguments)
     {
         if (arguments.Count != 1)
         {
             error = CellError.Value;
-            return Expression.Constant(CellError.Value);
+            return error;
         }
 
         var argument = arguments[0];
 
-        // Check for errors in the argument first
-        if (TryGetError(argument, out error))
+        if (TryGetError(argument, out var e))
         {
-            return Expression.Constant(error);
+            error = e;
+            return e;
         }
 
-        // Convert the argument to boolean expression following Excel semantics
-        var booleanExpr = ConvertToBooleanExpression(argument);
-
-        // Return the logical NOT of the boolean expression
-        return Expression.Not(booleanExpr);
+        return !ConvertToBoolean(argument);
     }
 }
