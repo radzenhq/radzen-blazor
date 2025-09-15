7a54ee141da697551d4de563709149ec0850d966
Atanas Korchev <akorchev@gmail.com>
2025-09-15 21:08:11 +0300

    Introduce CellData. Use it for formula calculation. Unify formula behavior with Excel.

--- Radzen.Blazor/Spreadsheet/Functions/ErrorFunction.cs
+++ Radzen.Blazor/Spreadsheet/Functions/ErrorFunction.cs
@@ -1,18 +1,10 @@
-using System;
 using System.Collections.Generic;
 
 #nullable enable
 
 namespace Radzen.Blazor.Spreadsheet;
 
-/// <summary>
-/// Implements an error function for handling missing or invalid formula functions.
-/// </summary>
 class ErrorFunction : FormulaFunction
 {
-    public override object? Evaluate(List<object?> arguments)
-    {
-        error = CellError.Name;
-        return error;
-    }
+    public override CellData Evaluate(List<CellData> arguments) => CellData.FromError(CellError.Name);
 }
