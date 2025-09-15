eab3d33bfc94f08a5c2928de1accff3849a785f9
Atanas Korchev <akorchev@gmail.com>
2025-09-15 17:45:12 +0300

    Remove expressions from formulas.

--- Radzen.Blazor/Spreadsheet/Functions/ErrorFunction.cs
+++ Radzen.Blazor/Spreadsheet/Functions/ErrorFunction.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq.Expressions;
 
 #nullable enable
 
@@ -11,25 +10,9 @@ namespace Radzen.Blazor.Spreadsheet;
 /// </summary>
 class ErrorFunction : FormulaFunction
 {
-    private readonly string functionName;
-
-    /// <summary>
-    /// Initializes a new instance of the ErrorFunction class.
-    /// </summary>
-    /// <param name="functionName">The name of the function that was not found.</param>
-    public ErrorFunction(string functionName)
-    {
-        this.functionName = functionName;
-    }
-
-    /// <summary>
-    /// Evaluates the error function by returning a #NAME? error.
-    /// </summary>
-    /// <param name="arguments">The function arguments (ignored).</param>
-    /// <returns>An expression representing a #NAME? error.</returns>
-    public override Expression Evaluate(List<Expression> arguments)
+    public override object? Evaluate(List<object?> arguments)
     {
         error = CellError.Name;
-        return Expression.Constant(CellError.Name);
+        return error;
     }
 }
