f4d916a0dea9a10c048bf48e6d2885973bd9f3ce
Atanas Korchev <akorchev@gmail.com>
2025-09-16 09:20:26 +0300

    Add support for SUMIF. Describe function parameters.

--- Radzen.Blazor/Spreadsheet/Functions/CountAllFunction.cs
+++ Radzen.Blazor/Spreadsheet/Functions/CountAllFunction.cs
@@ -1,21 +1,26 @@
-using System.Collections.Generic;
-
 #nullable enable
 
 namespace Radzen.Blazor.Spreadsheet;
 
 class CountAllFunction : FormulaFunction
 {
-    public override CellData Evaluate(List<CellData> arguments)
+    public override FunctionParameter[] Parameters => 
+    [
+        new ("value", ParameterType.Sequence, isRequired: true)
+    ];
+
+    public override CellData Evaluate(FunctionArguments arguments)
     {
-        if (arguments.Count == 0)
+        var values = arguments.GetSequence("value");
+
+        if (values == null || values.Count == 0)
         {
             return CellData.FromNumber(0d);
         }
 
         var count = 0d;
 
-        foreach (var v in arguments)
+        foreach (var v in values)
         {
             if (v.IsEmpty)
             {
---
+++ Radzen.Blazor/Spreadsheet/Functions/FunctionArguments.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+
+#nullable enable
+
+namespace Radzen.Blazor.Spreadsheet;
+
+/// <summary>
+/// Represents the arguments passed to a formula function, organized by parameter name.
+/// </summary>
+public class FunctionArguments
+{
+    private readonly Dictionary<string, object> arguments = [];
+
+    /// <summary>
+    /// Sets an argument value for the specified parameter name.
+    /// </summary>
+    /// <param name="parameterName">The name of the parameter.</param>
+    /// <param name="value">The argument value.</param>
+    public void Set(string parameterName, object value)
+    {
+        arguments[parameterName] = value;
+    }
+
+    private object? Get(string parameterName)
+    {
+        return arguments.TryGetValue(parameterName, out var value) ? value : null;
+    }
+
+    /// <summary>
+    /// Gets a single CellData value for the specified parameter name.
+    /// </summary>
+    /// <param name="parameterName">The name of the parameter.</param>
+    /// <returns>The CellData value, or null if not found.</returns>
+    public CellData? GetSingle(string parameterName)
+    {
+        var value = Get(parameterName);
+        return value as CellData;
+    }
+
+    /// <summary>
+    /// Gets a range of CellData values for the specified parameter name.
+    /// </summary>
+    /// <param name="parameterName">The name of the parameter.</param>
+    /// <returns>The list of CellData values, or null if not found.</returns>
+    public List<CellData>? GetRange(string parameterName)
+    {
+        var value = Get(parameterName);
+        return value as List<CellData>;
+    }
+
+    /// <summary>
+    /// Gets a sequence of CellData values for the specified parameter name.
+    /// </summary>
+    /// <param name="parameterName">The name of the parameter.</param>
+    /// <returns>The list of CellData values, or null if not found.</returns>
+    public List<CellData>? GetSequence(string parameterName)
+    {
+        var value = Get(parameterName);
+        return value as List<CellData>;
+    }
+}
