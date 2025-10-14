f107e16601175d94eaf251245aee0bd2e04667a6
Atanas Korchev <akorchev@gmail.com>
2025-10-14 20:04:23 +0300

    COLUMN function.

--- Radzen.Blazor/Spreadsheet/Functions/FunctionStore.cs
+++ Radzen.Blazor/Spreadsheet/Functions/FunctionStore.cs
@@ -35,6 +35,7 @@ public class FunctionStore
         Add<AndFunction>();
         Add<OrFunction>();
         Add<NotFunction>();
+        Add<ColumnFunction>();
         Add<RowFunction>();
         Add<MaxFunction>();
         Add<MaxAllFunction>();
