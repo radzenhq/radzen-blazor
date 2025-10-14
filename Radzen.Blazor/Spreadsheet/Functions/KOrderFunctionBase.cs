34f9e688d804d48a9e3ed5fbccd2c90ce78103f2
Atanas Korchev <akorchev@gmail.com>
2025-10-14 11:03:20 +0300

    LARGE and SMALL functions.

---
+++ Radzen.Blazor/Spreadsheet/Functions/KOrderFunctionBase.cs
@@ -0,0 +1,70 @@
+#nullable enable
+
+using System.Collections.Generic;
+
+namespace Radzen.Blazor.Spreadsheet;
+
+abstract class KOrderFunctionBase : FormulaFunction
+{
+    protected abstract double GetResult(List<double> numbers, int k);
+
+    public override FunctionParameter[] Parameters =>
+    [
+        new("array", ParameterType.Collection, isRequired: true),
+        new("k", ParameterType.Single, isRequired: true)
+    ];
+
+    public override CellData Evaluate(FunctionArguments arguments)
+    {
+        var arrayArg = arguments.GetRange("array");
+        var kArg = arguments.GetSingle("k");
+
+        if (arrayArg == null || kArg == null)
+        {
+            return CellData.FromError(CellError.Value);
+        }
+
+        if (kArg.IsError)
+        {
+            return kArg;
+        }
+
+        var k = kArg.GetValueOrDefault<int?>();
+
+        if (k is null)
+        {
+            return CellData.FromError(CellError.Value);
+        }
+
+        var numbers = new List<double>();
+
+        foreach (var cell in arrayArg)
+        {
+            if (cell.IsError)
+            {
+                return cell;
+            }
+
+            if (cell.Type == CellDataType.Number)
+            {
+                numbers.Add(cell.GetValueOrDefault<double>());
+            }
+        }
+
+        if (numbers.Count == 0)
+        {
+            return CellData.FromError(CellError.Num);
+        }
+
+        if (k.Value <= 0 || k.Value > numbers.Count)
+        {
+            return CellData.FromError(CellError.Num);
+        }
+
+        numbers.Sort();
+
+        var result = GetResult(numbers, k.Value);
+
+        return CellData.FromNumber(result);
+    }
+}
