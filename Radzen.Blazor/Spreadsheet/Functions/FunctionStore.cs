11863e2f9133697b65aa383d8267ea6cee5c82b5
Atanas Korchev <akorchev@gmail.com>
2025-10-16 15:14:08 +0300

    LOWER function.

--- Radzen.Blazor/Spreadsheet/Functions/FunctionStore.cs
+++ Radzen.Blazor/Spreadsheet/Functions/FunctionStore.cs
@@ -72,6 +72,7 @@ public class FunctionStore
         Add<SubstituteFunction>();
         Add<ProperFunction>();
         Add<UpperFunction>();
+        Add<LowerFunction>();
     }
 
     /// <summary>
