78d41629af8f1a78b490be4f6781b1528eb0b6ea
Atanas Korchev <akorchev@gmail.com>
2025-10-17 11:31:55 +0300

    MONTH function.

--- Radzen.Blazor/Spreadsheet/Functions/DayFunction.cs
+++ Radzen.Blazor/Spreadsheet/Functions/DayFunction.cs
@@ -4,41 +4,9 @@ using System;
 
 namespace Radzen.Blazor.Spreadsheet;
 
-class DayFunction : FormulaFunction
+class DayFunction : DatePartFunctionBase
 {
     public override string Name => "DAY";
 
-    public override FunctionParameter[] Parameters =>
-    [
-        new("serial_number", ParameterType.Single, isRequired: true)
-    ];
-
-    public override CellData Evaluate(FunctionArguments arguments)
-    {
-        var arg = arguments.GetSingle("serial_number");
-        if (arg == null)
-        {
-            return CellData.FromError(CellError.Value);
-        }
-
-        if (arg.IsError)
-        {
-            return arg;
-        }
-
-        // Accept date value or numeric serial (including fractional)
-        if (arg.Type == CellDataType.Date)
-        {
-            var dt = arg.GetValueOrDefault<DateTime>();
-            return CellData.FromNumber(dt.Day);
-        }
-
-        if (arg.TryCoerceToNumber(out var serial, allowBooleans: false, nonNumericTextAsZero: false))
-        {
-            var dt = serial.ToDate();
-            return CellData.FromNumber(dt.Day);
-        }
-
-        return CellData.FromError(CellError.Value);
-    }
+    protected override int GetPart(DateTime dateTime) => dateTime.Day;
 }
