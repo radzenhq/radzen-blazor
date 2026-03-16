5de708c90e73856ad8757c9d6d1a65bd6854d435
Atanas Korchev <akorchev@gmail.com>
2026-03-16 15:18:54 +0200

    EOMONTH function.

---
+++ Radzen.Blazor/Spreadsheet/Functions/EomonthFunction.cs
@@ -0,0 +1,48 @@
+#nullable enable
+
+using System;
+
+namespace Radzen.Blazor.Spreadsheet;
+
+class EomonthFunction : FormulaFunction
+{
+    public override string Name => "EOMONTH";
+
+    public override FunctionParameter[] Parameters =>
+    [
+        new("start_date", ParameterType.Single, isRequired: true),
+        new("months", ParameterType.Single, isRequired: true)
+    ];
+
+    public override CellData Evaluate(FunctionArguments arguments)
+    {
+        var startArg = arguments.GetSingle("start_date");
+
+        if (startArg == null || startArg.IsError)
+        {
+            return startArg ?? CellData.FromError(CellError.Value);
+        }
+
+        if (!startArg.TryCoerceToDate(out var startDate))
+        {
+            return CellData.FromError(CellError.Value);
+        }
+
+        if (!TryGetInteger(arguments, "months", true, null, out var months, out var error))
+        {
+            return error!;
+        }
+
+        try
+        {
+            var target = startDate.AddMonths(months);
+            var endOfMonth = new DateTime(target.Year, target.Month, DateTime.DaysInMonth(target.Year, target.Month));
+
+            return CellData.FromDate(endOfMonth);
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            return CellData.FromError(CellError.Num);
+        }
+    }
+}
