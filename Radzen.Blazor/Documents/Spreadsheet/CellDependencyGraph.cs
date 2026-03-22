18a0f9fba50a1583ae9be688241e56c4ffc5b07f
Atanas Korchev <akorchev@gmail.com>
2026-03-22 15:06:09 +0200

    Fix range dependency tracking to include all cells, not just endpoints

--- Radzen.Blazor/Documents/Spreadsheet/CellDependencyGraph.cs
+++ Radzen.Blazor/Documents/Spreadsheet/CellDependencyGraph.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -60,7 +61,7 @@ internal class CellDependencyGraph
             return;
         }
 
-        var tree = FormulaParser.Parse(cell.Formula);
+        var tree = cell.FormulaSyntaxTree ?? FormulaParser.Parse(cell.Formula);
         var visitor = new DependencyVisitor(cell.Worksheet);
         tree.Root.Accept(visitor);
 
@@ -124,28 +125,34 @@ class DependencyVisitor(Worksheet sheet) : IFormulaSyntaxNodeVisitor
         binaryExpressionSyntaxNode.Right.Accept(this);
     }
 
+    private Worksheet? ResolveSheet(string? worksheetName)
+    {
+        if (string.IsNullOrEmpty(worksheetName))
+        {
+            return sheet;
+        }
+
+        var target = sheet.Workbook.GetSheet(worksheetName);
+
+        return target?.Name == worksheetName ? target : null;
+    }
+
     public void VisitCell(CellSyntaxNode cellIdentifierSyntaxNode)
     {
         var address = cellIdentifierSyntaxNode.Token.Address;
-        var targetSheet = sheet;
+        var targetSheet = ResolveSheet(address.Worksheet);
 
-        if (!string.IsNullOrEmpty(address.Worksheet))
+        if (targetSheet == null)
         {
-            var wb = sheet.Workbook;
-            targetSheet = wb.GetSheet(address.Worksheet) ?? targetSheet;
-            if (targetSheet.Name != address.Worksheet)
-            {
-                return;
-            }
+            return;
         }
 
         if (address.Row >= targetSheet.RowCount || address.Column >= targetSheet.ColumnCount)
         {
-            // Out of bounds, do not add dependency
             return;
         }
-        var cell = targetSheet.Cells[address];
-        Dependencies.Add(cell);
+
+        Dependencies.Add(targetSheet.Cells[address]);
     }
 
     public void VisitFunction(FunctionSyntaxNode functionSyntaxNode)
@@ -158,7 +165,31 @@ class DependencyVisitor(Worksheet sheet) : IFormulaSyntaxNodeVisitor
 
     public void VisitRange(RangeSyntaxNode rangeSyntaxNode)
     {
-        rangeSyntaxNode.Start.Accept(this);
-        rangeSyntaxNode.End.Accept(this);
+        var startAddress = rangeSyntaxNode.Start.Token.Address;
+        var endAddress = rangeSyntaxNode.End.Token.Address;
+        var targetSheet = ResolveSheet(startAddress.Worksheet);
+
+        if (targetSheet == null)
+        {
+            return;
+        }
+
+        var startRow = Math.Min(startAddress.Row, endAddress.Row);
+        var endRow = Math.Max(startAddress.Row, endAddress.Row);
+        var startCol = Math.Min(startAddress.Column, endAddress.Column);
+        var endCol = Math.Max(startAddress.Column, endAddress.Column);
+
+        for (var row = startRow; row <= endRow; row++)
+        {
+            for (var col = startCol; col <= endCol; col++)
+            {
+                if (row >= targetSheet.RowCount || col >= targetSheet.ColumnCount)
+                {
+                    continue;
+                }
+
+                Dependencies.Add(targetSheet.Cells[new CellRef(row, col)]);
+            }
+        }
     }
 }
