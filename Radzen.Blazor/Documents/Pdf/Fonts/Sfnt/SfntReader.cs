5f55539d452dc260178329f3edba8dd8470a3be5
Atanas Korchev <akorchev@gmail.com>
2026-07-10 12:46:00 +0300

    Sfnt parser with ttc and cmap 4/12 support

---
+++ Radzen.Blazor/Documents/Pdf/Fonts/Sfnt/SfntReader.cs
@@ -0,0 +1,84 @@
+#nullable enable
+using System;
+using System.IO;
+
+namespace Radzen.Documents.Pdf.Fonts.Sfnt
+{
+    // Big-endian primitive reader over a byte[] with a movable cursor.
+    internal sealed class SfntReader
+    {
+        private readonly byte[] data;
+
+        public SfntReader(byte[] data, int position = 0)
+        {
+            this.data = data ?? throw new ArgumentNullException(nameof(data));
+            Position = position;
+        }
+
+        public int Position { get; set; }
+
+        public int Length => data.Length;
+
+        private void Require(int count)
+        {
+            if (Position < 0 || Position + count > data.Length)
+            {
+                throw new InvalidDataException("Attempt to read past the end of the sfnt data.");
+            }
+        }
+
+        public byte ReadByte()
+        {
+            Require(1);
+            return data[Position++];
+        }
+
+        public ushort ReadUInt16()
+        {
+            Require(2);
+            var value = (ushort)((data[Position] << 8) | data[Position + 1]);
+            Position += 2;
+            return value;
+        }
+
+        public short ReadInt16() => (short)ReadUInt16();
+
+        public uint ReadUInt32()
+        {
+            Require(4);
+            var value = ((uint)data[Position] << 24)
+                | ((uint)data[Position + 1] << 16)
+                | ((uint)data[Position + 2] << 8)
+                | data[Position + 3];
+            Position += 4;
+            return value;
+        }
+
+        public int ReadInt32() => (int)ReadUInt32();
+
+        public string ReadTag()
+        {
+            Require(4);
+            var chars = new char[4];
+            for (var i = 0; i < 4; i++)
+            {
+                chars[i] = (char)data[Position + i];
+            }
+
+            Position += 4;
+            return new string(chars);
+        }
+
+        public ushort ReadUInt16At(int offset)
+        {
+            Position = offset;
+            return ReadUInt16();
+        }
+
+        public short ReadInt16At(int offset)
+        {
+            Position = offset;
+            return ReadInt16();
+        }
+    }
+}
