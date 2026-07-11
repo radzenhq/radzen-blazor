3c108ba5f2c76eb9203cb8ac7fac800b8818322e
Atanas Korchev <akorchev@gmail.com>
2026-07-12 01:21:52 +0300

    Throw NotSupportedException on RTL/vertical section until shaping is implemented (no silent LTR)

---
+++ Radzen.Blazor.Tests/Pdf/SectionDirectionSupportTests.cs
@@ -0,0 +1,35 @@
+using Radzen.Documents.Pdf;
+using Xunit;
+
+namespace Radzen.Blazor.Pdf.Tests;
+
+#nullable enable
+
+// The engine does not implement RTL / vertical shaping yet; setting a non-default
+// Direction or WritingMode must fail loudly at build rather than silently laying out LTR.
+public class SectionDirectionSupportTests
+{
+    [Fact]
+    public void DefaultSection_Builds()
+    {
+        var builder = new DocumentBuilder();
+        builder.Sections.Add();
+        Assert.NotEmpty(builder.ToArray());
+    }
+
+    [Fact]
+    public void RightToLeftDirection_ThrowsOnBuild()
+    {
+        var builder = new DocumentBuilder();
+        builder.Sections.Add().Direction = FlowDirection.RightToLeft;
+        Assert.Throws<System.NotSupportedException>(() => builder.ToArray());
+    }
+
+    [Fact]
+    public void VerticalWritingMode_ThrowsOnBuild()
+    {
+        var builder = new DocumentBuilder();
+        builder.Sections.Add().WritingMode = WritingMode.VerticalRightToLeft;
+        Assert.Throws<System.NotSupportedException>(() => builder.ToArray());
+    }
+}
